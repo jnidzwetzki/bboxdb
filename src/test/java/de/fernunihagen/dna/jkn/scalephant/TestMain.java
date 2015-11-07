e84e6214d57eaa11979c73a767b4daff35915bdc
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2015-11-07 13:17:16 +0100

    Added junit to the project

---
+++ src/test/java/de/fernunihagen/dna/jkn/scalephant/TestMain.java
@@ -0,0 +1,13 @@
+package de.fernunihagen.dna.jkn.scalephant;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TestMain {
+	
+	@Test
+	public void testPrintHelloWorld() {
+		Assert.assertEquals("abc", "abc");
+	}
+}
