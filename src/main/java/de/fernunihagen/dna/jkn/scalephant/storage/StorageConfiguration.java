0e30010f19373c45c5c1467de432700143b22224
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2015-11-16 21:07:41 +0100

    Added directories

--- src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/StorageConfiguration.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/StorageConfiguration.java
@@ -8,6 +8,12 @@ public class StorageConfiguration {
 	// 10000 Entries
 	private final static int MEMTABLE_ENTRIES = 10000;
 	
+	// Data dir
+	private final static String DATA_DIR = "/tmp/scalephant/data";
+	
+	// Commit log dir
+	private final static String COMMITLOG_DIR = "/tmp/scalephant/commitlog";
+	
 	/**
 	 * The size of the in memory tuple buffer
 	 * 
@@ -20,10 +26,28 @@ public class StorageConfiguration {
 	/**
 	 * The maximal amount of entries in the memtable
 	 * 
-	 * @return maximal amount
+	 * @return maximal amount of enties
 	 */
 	public int getMemtableEntries() {
 		return MEMTABLE_ENTRIES;
+	}
+	
+	/**
+	 * Get the directory for storing the SSTables
+	 * 
+	 * @return name of the data directory
+	 */
+	public static String getDataDir() {
+		return DATA_DIR;
+	}
+	
+	/**
+	 * Get the name of the commit log
+	 * 
+	 * @return name of the commit log
+	 */
+	public static String getCommitlogDir() {
+		return COMMITLOG_DIR;
 	}
 	
 }
