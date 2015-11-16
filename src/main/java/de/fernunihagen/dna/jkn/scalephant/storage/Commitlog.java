db6460a6063fcc040739b7cea025976915f2004f
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2015-11-16 21:24:30 +0100

    Added commit log type

---
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/Commitlog.java
@@ -0,0 +1,11 @@
+package de.fernunihagen.dna.jkn.scalephant.storage;
+
+public class Commitlog {
+	
+	protected final String name;
+	
+	public Commitlog(final String name) {
+		this.name = name;
+	}
+
+}
