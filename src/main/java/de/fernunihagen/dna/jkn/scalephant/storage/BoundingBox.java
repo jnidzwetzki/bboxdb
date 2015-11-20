f246565d6d721a97fa66edacbe8f35f0a6559cde
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2015-11-20 14:41:37 +0100

    Added constructor and empty box

--- src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/BoundingBox.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/BoundingBox.java
@@ -1,8 +1,14 @@
 package de.fernunihagen.dna.jkn.scalephant.storage;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import de.fernunihagen.dna.jkn.scalephant.storage.sstable.SSTableHelper;
+
 public class BoundingBox {
+	
+	public final static BoundingBox EMPTY_BOX = new BoundingBox();
 	
 	/**
 	 * The variable boundingBox contains a bounding box for a tuple
@@ -14,7 +20,20 @@ public class BoundingBox {
 	 * boundingBox[n-1] = x_n
 	 * boundingBox[n] = y_n
 	 */
-	protected List<Short> boundingBox;
+	protected final List<Long> boundingBox;
+	
+	public BoundingBox(Long... args) {
+		boundingBox = new ArrayList<Long>(args.length);
+		boundingBox.addAll(Arrays.asList(args));
+	}
+	
+	public BoundingBox(long[] values) {
+		boundingBox = new ArrayList<Long>(values.length);
+		
+		for(int i = 0; i < values.length; i++) {
+			boundingBox.add(values[i]);
+		}
+	}
 	
 	/**
 	 * Checks that the bounding box is valid
@@ -32,6 +51,20 @@ public class BoundingBox {
 	 */
 	public int getSize() {
 		return boundingBox.size();
+	}
+	
+	/**
+	 * Convert the bounding box into a byte array
+	 * 
+	 * @return
+	 */
+	public byte[] toByteArray() {
+		final long[] values = new long[boundingBox.size()];
+		for(int i = 0; i < boundingBox.size(); i++) {
+			values[i] = boundingBox.get(i);
+		}
+		
+		return SSTableHelper.longArrayToByteBuffer(values).array();
 	}
 	
 }
