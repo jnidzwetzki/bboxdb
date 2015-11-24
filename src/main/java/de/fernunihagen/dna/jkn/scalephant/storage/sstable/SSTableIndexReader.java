d09a27ea459ee594f60f140f5fa46f62ae8e9415
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2015-11-24 19:45:42 +0100

    Switched to memory mapped io

--- src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/sstable/SSTableIndexReader.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/sstable/SSTableIndexReader.java
@@ -1,21 +1,65 @@
 package de.fernunihagen.dna.jkn.scalephant.storage.sstable;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.fernunihagen.dna.jkn.scalephant.Lifecycle;
 import de.fernunihagen.dna.jkn.scalephant.storage.StorageManagerException;
 
-public class SSTableIndexReader extends AbstractTableReader {
+public class SSTableIndexReader implements Lifecycle {
 	
+	/**
+	 * The number of the table
+	 */
+	protected final int tablebumber;
+	
+	/**
+	 * The name of the table
+	 */
+	protected final String name;
+	
+	/**
+	 * The filename of the table
+	 */
+	protected final File file;
+	
+	/**
+	 * The Directoy for the SSTables
+	 */
+	protected final String directory;
+	
+	/**
+	 * The Logger
+	 */
+	protected static final Logger logger = LoggerFactory.getLogger(AbstractTableReader.class);
+	
+	/**
+	 * The coresponding sstable reader
+	 */
 	protected final SSTableReader sstableReader;
 	
-	protected byte[] positionBytes = new byte[SSTableHelper.LONG_BYTES];
+	/**
+	 * The memory region
+	 */
+	private MappedByteBuffer memory;
+
+	/**
+	 * The coresponding fileChanel
+	 */
+	private FileChannel fileChannel;
 
 	public SSTableIndexReader(final SSTableReader sstableReader) throws StorageManagerException {
-		super(sstableReader.getName(), 
-				sstableReader.getDirectory(), 
-				constructFilenameFromReader(sstableReader));
-		
+		this.name = sstableReader.getName();
+		this.directory = sstableReader.getDirectory();
+		this.file = constructFileFromReader(sstableReader);
+		this.tablebumber = SSTableHelper.extractSequenceFromFilename(name, file.getName());
 		this.sstableReader = sstableReader;
 	}
 
@@ -24,7 +68,7 @@ public class SSTableIndexReader extends AbstractTableReader {
 	 * @param sstableReader
 	 * @return
 	 */
-	protected static File constructFilenameFromReader(
+	protected static File constructFileFromReader(
 			final SSTableReader sstableReader) {
 		
 		return new File(SSTableManager.getSSTableIndexFilename(
@@ -43,7 +87,6 @@ public class SSTableIndexReader extends AbstractTableReader {
 	public long getPositionForTuple(final String key) throws StorageManagerException {
 		
 		try {
-			resetFileReaderPosition();
 			
 		/*	long firstEntry = 0;
 			long lastEntry = fileInputStream.getChannel().size() - SSTableConst.MAGIC_BYTES.length / SSTableConst.INDEX_ENTRY_BYTES;
@@ -51,12 +94,16 @@ public class SSTableIndexReader extends AbstractTableReader {
 			long curEntry = (long) ((lastEntry - firstEntry) / 2.0);
 			*/
 			
-			while(reader.available() > 0) {
-				long position = decodeKeyPosition();
+			memory.position(SSTableConst.MAGIC_BYTES.length);
+			
+			while(memory.hasRemaining()) {
+				memory.order(SSTableConst.SSTABLE_BYTE_ORDER);
+				long position = memory.getLong();
+
 				final String decodedKey = sstableReader.decodeOnlyKeyFromTupleAtPosition(position);
 				
 				if(decodedKey.equals(key)) {
-					return position;
+					return position;	
 				}
 				
 				if(decodedKey.compareTo(key) > 0) {
@@ -71,10 +118,31 @@ public class SSTableIndexReader extends AbstractTableReader {
 		
 		return -1;
 	}
-	
-	protected long decodeKeyPosition() throws IOException {
-		reader.read(positionBytes, 0, positionBytes.length);
-		return SSTableHelper.readLongFromByteBuffer(positionBytes);
+
+	@Override
+	public void init() {
+		try {
+			fileChannel = new RandomAccessFile(file, "r").getChannel();
+			memory = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+		} catch (FileNotFoundException e) {
+			logger.error("Error during an IO operation", e);
+		} catch (IOException e) {
+			logger.error("Error during an IO operation", e);
+		}
+		
+	}
+
+	@Override
+	public void shutdown() {
+		
+		if(fileChannel != null) {
+			try {
+				fileChannel.close();
+				fileChannel = null;
+			} catch (IOException e) {
+				logger.error("Error during an IO operation", e);
+			}
+		}
 	}
 
 }
