9abc2c61a79b914f8069a218c6f9b1e5f33e4271
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2015-11-26 14:15:38 +0100

    Added some basic tests for the sstable writer and reader

---
+++ src/test/java/de/fernunihagen/dna/jkn/scalephant/TestSSTable.java
@@ -0,0 +1,99 @@
+package de.fernunihagen.dna.jkn.scalephant;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import de.fernunihagen.dna.jkn.scalephant.storage.BoundingBox;
+import de.fernunihagen.dna.jkn.scalephant.storage.StorageConfiguration;
+import de.fernunihagen.dna.jkn.scalephant.storage.StorageInterface;
+import de.fernunihagen.dna.jkn.scalephant.storage.StorageManager;
+import de.fernunihagen.dna.jkn.scalephant.storage.Tuple;
+import de.fernunihagen.dna.jkn.scalephant.storage.sstable.SSTableIndexReader;
+import de.fernunihagen.dna.jkn.scalephant.storage.sstable.SSTableReader;
+import de.fernunihagen.dna.jkn.scalephant.storage.sstable.SSTableWriter;
+
+public class TestSSTable {
+	
+	protected final static String TEST_RELATION = "testrelation";
+	protected final StorageConfiguration storageConfiguration = new StorageConfiguration();
+
+	
+	/**
+	 * Test written files
+	 * @throws Exception
+	 */
+	@Test
+	public void testWrittenFiles() throws Exception {
+		final StorageManager storageManager = StorageInterface.getStorageManager(TEST_RELATION);
+		storageManager.clear();
+		storageManager.shutdown();
+	
+		final List<Tuple> tupleList = createTupleList();
+		
+		final SSTableWriter ssTableWriter = new SSTableWriter(storageConfiguration.getDataDir(), TEST_RELATION, 1);
+		ssTableWriter.open();
+		ssTableWriter.addData(tupleList);
+		final File sstableFile = ssTableWriter.getSstableFile();
+		final File sstableIndexFile = ssTableWriter.getSstableIndexFile();
+		ssTableWriter.close();
+		
+		Assert.assertTrue(sstableFile.exists());
+		Assert.assertTrue(sstableIndexFile.exists());
+	}
+	
+	/**
+	 * Test the tuple iterator
+	 * @throws Exception
+	 */
+	@Test
+	public void testIndexIterator() throws Exception {
+		final StorageManager storageManager = StorageInterface.getStorageManager(TEST_RELATION);
+		storageManager.clear();
+		storageManager.shutdown();
+	
+		final List<Tuple> tupleList = createTupleList();
+		
+		final SSTableWriter ssTableWriter = new SSTableWriter(storageConfiguration.getDataDir(), TEST_RELATION, 1);
+		ssTableWriter.open();
+		ssTableWriter.addData(tupleList);
+		final File sstableFile = ssTableWriter.getSstableFile();
+		final File sstableIndexFile = ssTableWriter.getSstableIndexFile();
+		ssTableWriter.close();
+		
+		final SSTableReader sstableReader = new SSTableReader(TEST_RELATION, storageConfiguration.getDataDir(), sstableFile);
+		sstableReader.init();
+		final SSTableIndexReader ssTableIndexReader = new SSTableIndexReader(sstableReader);
+		ssTableIndexReader.init();
+		
+		Assert.assertEquals(sstableIndexFile, ssTableIndexReader.getFile());
+		
+		int tupleCounter = 0;
+		
+		for(Tuple tuple : ssTableIndexReader) {
+			Assert.assertEquals(tupleList.get(tupleCounter), tuple);
+			tupleCounter++;
+		}
+		
+		Assert.assertEquals(tupleList.size(), tupleCounter);
+	}
+
+	/**
+	 * Helper method for creating some test tuples
+	 * 
+	 * @return
+	 */
+	protected List<Tuple> createTupleList() {
+		final List<Tuple> tupleList = new ArrayList<Tuple>();
+		tupleList.add(new Tuple("1", BoundingBox.EMPTY_BOX, "abc".getBytes()));
+		tupleList.add(new Tuple("2", BoundingBox.EMPTY_BOX, "def".getBytes()));
+		tupleList.add(new Tuple("3", BoundingBox.EMPTY_BOX, "geh".getBytes()));
+		tupleList.add(new Tuple("4", BoundingBox.EMPTY_BOX, "ijk".getBytes()));
+		return tupleList;
+	}
+	
+}
