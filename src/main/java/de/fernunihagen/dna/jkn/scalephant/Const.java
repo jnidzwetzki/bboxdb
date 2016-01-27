8fae20644d4f2d1e94c6778098016619cfc8051e
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-01-27 17:19:57 +0100

    Added a dependency to apache commons deamon

---
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/Const.java
@@ -0,0 +1,7 @@
+package de.fernunihagen.dna.jkn.scalephant;
+
+public class Const {
+	
+	// The version of the software
+	public final static String VERSION = "0.0.3";
+}
--- src/main/java/de/fernunihagen/dna/jkn/scalephant/Main.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/Main.java
@@ -1,9 +1,36 @@
 package de.fernunihagen.dna.jkn.scalephant;
 
-public class Main {
+import org.apache.commons.daemon.Daemon;
+import org.apache.commons.daemon.DaemonContext;
+import org.apache.commons.daemon.DaemonInitException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	public static void main(String[] args) {
+public class Main implements Daemon {
 
+	/**
+	 * The Logger
+	 */
+	private final static Logger logger = LoggerFactory.getLogger(Main.class);
+
+	@Override
+	public void destroy() {
+		logger.info("Destroy the scalephant");
+	}
+
+	@Override
+	public void init(final DaemonContext ctx) throws DaemonInitException, Exception {
+		logger.info("Init the scalephant");
+	}
+
+	@Override
+	public void start() throws Exception {
+		logger.info("Starting up the scalephant - version: " + Const.VERSION);		
+	}
+
+	@Override
+	public void stop() throws Exception {
+		logger.info("Stopping the scalephant");
 	}
 
 }
