a45b4c7d4c971f57b82833bb349cdd037108de23
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-01-28 15:33:44 +0100

    Added some consts

--- src/main/java/de/fernunihagen/dna/jkn/scalephant/Const.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/Const.java
@@ -4,4 +4,11 @@ public class Const {
 	
 	// The version of the software
 	public final static String VERSION = "0.0.3";
+	
+	// The port for client requests
+	public final static int PORT = 50505;
+	
+	// The amount of threads to handle client connections
+	public final static int CLIENT_NETWORK_THREADS = 10;
+	
 }
