b0a4a03f82302ee56a0cd64f348d29ce3c4623ed
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-01-29 12:05:52 +0100

    Implemented a base connection handler

--- src/main/java/de/fernunihagen/dna/jkn/scalephant/Main.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/Main.java
@@ -6,31 +6,42 @@ import org.apache.commons.daemon.DaemonInitException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.fernunihagen.dna.jkn.scalephant.network.ConnectionHandler;
+
 public class Main implements Daemon {
 
 	/**
 	 * The Logger
 	 */
 	private final static Logger logger = LoggerFactory.getLogger(Main.class);
-
-	@Override
-	public void destroy() {
-		logger.info("Destroy the scalephant");
-	}
+	
+	/**
+	 * The network connection handler
+	 */
+	protected ConnectionHandler connectionHandler = null;
 
 	@Override
 	public void init(final DaemonContext ctx) throws DaemonInitException, Exception {
 		logger.info("Init the scalephant");
+		connectionHandler = new ConnectionHandler();
 	}
 
 	@Override
 	public void start() throws Exception {
 		logger.info("Starting up the scalephant - version: " + Const.VERSION);		
+		connectionHandler.init();
 	}
 
 	@Override
 	public void stop() throws Exception {
 		logger.info("Stopping the scalephant");
+		connectionHandler.shutdown();
+	}
+	
+	@Override
+	public void destroy() {
+		logger.info("Destroy the scalephant");
+		connectionHandler = null;
 	}
 	
 	public static void main(String[] args) throws DaemonInitException, Exception {
@@ -38,5 +49,4 @@ public class Main implements Daemon {
 		main.init(null);
 		main.start();
 	}
-
 }
---
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/network/ConnectionHandler.java
@@ -0,0 +1,150 @@
+package de.fernunihagen.dna.jkn.scalephant.network;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.fernunihagen.dna.jkn.scalephant.Const;
+import de.fernunihagen.dna.jkn.scalephant.Lifecycle;
+import de.fernunihagen.dna.jkn.scalephant.util.State;
+
+public class ConnectionHandler implements Lifecycle {
+	
+	/**
+	 * Our thread pool to handle connections
+	 */
+	protected ExecutorService threadPool;
+	
+	/**
+	 * The connection handler state
+	 */
+	protected final State state = new State(false);
+	
+	/**
+	 * The connection dispatcher runnable
+	 */
+	protected ConnectionDispatcher serverSocketDispatcher = null;
+	
+	/**
+	 * The thread that listens on the server socket and dispatches
+	 * incoming requests to the thread pool
+	 */
+	protected Thread serverSocketDispatchThread = null;
+	
+	/**
+	 * The Logger
+	 */
+	private final static Logger logger = LoggerFactory.getLogger(ConnectionHandler.class);
+	
+	/**
+	 * Start the network connection handler
+	 */
+	public void init() {
+		
+		if(state.isReady()) {
+			logger.info("init() called on ready instance, ignoring");
+			return;
+		}
+		
+		logger.info("Start the network connection handler on port: " + Const.PORT);
+		
+		if(threadPool == null) {
+			threadPool = Executors.newFixedThreadPool(Const.CLIENT_NETWORK_THREADS);
+		}
+		
+		serverSocketDispatcher = new ConnectionDispatcher();
+		serverSocketDispatchThread = new Thread(serverSocketDispatcher);
+		serverSocketDispatchThread.start();
+		
+		state.setReady(true);
+	}
+	
+	/**
+	 * Shutdown our thread pool
+	 */
+	public void shutdown() {
+		
+		logger.info("Shutdown the network connection handler");
+		
+		if(serverSocketDispatchThread != null) {
+			serverSocketDispatcher.setShutdown(true);
+			serverSocketDispatchThread.interrupt();
+			
+			serverSocketDispatchThread = null;
+			serverSocketDispatcher = null;
+		}
+		
+		if(threadPool != null) {
+			threadPool.shutdown();
+		}
+		
+		threadPool = null;
+		state.setReady(false);
+	}
+	
+	@Override
+	protected void finalize() throws Throwable {
+		shutdown();
+		super.finalize();
+	}
+	
+	/**
+	 * Is the connection handler ready?
+	 * @return
+	 */
+	public boolean isReady() {
+		return state.isReady();
+	}
+	
+	
+	/**
+	 * The connection dispatcher
+	 *
+	 */
+	class ConnectionDispatcher implements Runnable {
+
+		/**
+		 * The shutdown signal
+		 */
+		protected volatile boolean shutdown = false;
+
+		@Override
+		public void run() {
+			
+			logger.info("Starting new connection dispatcher");
+			
+			try {
+				final ServerSocket serverSocket = new ServerSocket(Const.PORT);
+				
+				while(! shutdown) {
+					final Socket clientSocket = serverSocket.accept();
+					handleConnection(clientSocket);
+				}
+				
+				serverSocket.close();
+			} catch(IOException e) {
+				logger.error("Unable to start server socket ", e);
+				shutdown = true;
+			}
+			
+			logger.info("Shutting down the connection dispatcher");
+		}
+		
+		/**
+		 * Set the shutdown flag
+		 * @param shutdown
+		 */
+		public void setShutdown(boolean shutdown) {
+			this.shutdown = shutdown;
+		}
+		
+		protected void handleConnection(final Socket clientSocket) {
+			logger.debug("Handling new connection from: " + clientSocket.getInetAddress());
+		}
+	}
+}
