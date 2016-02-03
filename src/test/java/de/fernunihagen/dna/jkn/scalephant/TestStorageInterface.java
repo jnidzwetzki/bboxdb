c763b6b0d67e52b3c13be6e7dcfdd7549075b78d
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-02-03 14:40:49 +0100

    Extracted relation name

--- src/test/java/de/fernunihagen/dna/jkn/scalephant/TestStorageInterface.java
+++ src/test/java/de/fernunihagen/dna/jkn/scalephant/TestStorageInterface.java
@@ -9,15 +9,21 @@ import de.fernunihagen.dna.jkn.scalephant.storage.StorageInterface;
 public class TestStorageInterface {
 	
 	/**
+	 * The name of the test relation
+	 */
+	protected static final String RELATION_NAME = "test_storage";
+
+	/**
 	 * Test registering and unregistering the storage manager
+	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void testRegisterAndUnregister() {
-		Assert.assertFalse(StorageInterface.isStorageManagerActive("test_storage"));
-		StorageInterface.getStorageManager("test_storage");
-		Assert.assertTrue(StorageInterface.isStorageManagerActive("test_storage"));
-		StorageInterface.shutdown("test_storage");
-		Assert.assertFalse(StorageInterface.isStorageManagerActive("test_storage"));
+		Assert.assertFalse(StorageInterface.isStorageManagerActive(RELATION_NAME));
+		StorageInterface.getStorageManager(RELATION_NAME);
+		Assert.assertTrue(StorageInterface.isStorageManagerActive(RELATION_NAME));
+		StorageInterface.shutdown(RELATION_NAME);
+		Assert.assertFalse(StorageInterface.isStorageManagerActive(RELATION_NAME));
 	}
 }
