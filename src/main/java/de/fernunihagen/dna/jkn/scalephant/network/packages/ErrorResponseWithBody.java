22c595bed5acb4449bb35f46ae80bd58a7cf2a14
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-02-09 10:51:03 +0100

    Added further result packages

---
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/network/packages/ErrorResponseWithBody.java
@@ -0,0 +1,97 @@
+package de.fernunihagen.dna.jkn.scalephant.network.packages;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.fernunihagen.dna.jkn.scalephant.network.NetworkConst;
+import de.fernunihagen.dna.jkn.scalephant.network.NetworkPackageDecoder;
+import de.fernunihagen.dna.jkn.scalephant.network.NetworkPackageEncoder;
+
+public class ErrorResponseWithBody extends NetworkResponsePackage {
+	
+	/**
+	 * The result body
+	 */
+	protected final String body;
+
+	/**
+	 * The Logger
+	 */
+	private final static Logger logger = LoggerFactory.getLogger(ErrorResponseWithBody.class);
+
+	public ErrorResponseWithBody(final short sequenceNumber, final String body) {
+		super(sequenceNumber);
+		this.body = body;
+	}
+
+	@Override
+	public byte getPackageType() {
+		return NetworkConst.RESPONSE_ERROR_WITH_BODY;
+	}
+
+	@Override
+	public byte[] getByteArray() {
+		final NetworkPackageEncoder networkPackageEncoder 
+			= new NetworkPackageEncoder();
+	
+		final ByteArrayOutputStream bos = networkPackageEncoder.getOutputStreamForResponsePackage(sequenceNumber, getPackageType());
+		
+		try {
+			final byte[] bodyBytes = body.getBytes();
+			final ByteBuffer bb = ByteBuffer.allocate(2);
+			bb.order(NetworkConst.NETWORK_BYTEORDER);
+			bb.putShort((short) bodyBytes.length);
+			
+			// Write body length
+			final int bodyLength = bb.capacity() + bodyBytes.length;
+			
+			final ByteBuffer bodyLengthBuffer = ByteBuffer.allocate(4);
+			bodyLengthBuffer.order(NetworkConst.NETWORK_BYTEORDER);
+			bodyLengthBuffer.putInt(bodyLength);
+			bos.write(bodyLengthBuffer.array());
+			
+			// Write body
+			bos.write(bb.array());
+			bos.write(bodyBytes);
+			
+			bos.close();
+		} catch (IOException e) {
+			logger.error("Got exception while converting package into bytes", e);
+			return null;
+		}
+	
+		return bos.toByteArray();
+	}
+	
+	
+	/**
+	 * Decode the encoded package into a object
+	 * 
+	 * @param encodedPackage
+	 * @return
+	 */
+	public static ErrorResponseWithBody decodeTuple(final byte encodedPackage[]) {
+		final ByteBuffer bb = NetworkPackageDecoder.encapsulateBytes(encodedPackage);
+		
+		NetworkPackageDecoder.validateResponsePackageHeader(bb, NetworkConst.RESPONSE_ERROR_WITH_BODY);
+
+		short bodyLength = bb.getShort();
+		
+		final byte[] bodyBytes = new byte[bodyLength];
+		bb.get(bodyBytes, 0, bodyBytes.length);
+		final String body = new String(bodyBytes);
+		
+		if(bb.remaining() != 0) {
+			logger.error("Some bytes are left after encoding: " + bb.remaining());
+		}
+		
+		final short requestId = NetworkPackageDecoder.geRequestIDFromResponsePackage(bb);
+		
+		return new ErrorResponseWithBody(requestId, body);
+	}
+
+}
