c4d0ba0748e2f0fa902a9885922c4067e1e708df
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-02-17 13:37:04 +0100

    Added tests for the tablename parser

---
+++ src/test/java/de/fernunihagen/dna/jkn/scalephant/TestTablenameParser.java
@@ -0,0 +1,60 @@
+package de.fernunihagen.dna.jkn.scalephant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import de.fernunihagen.dna.jkn.scalephant.util.Tablename;
+
+public class TestTablenameParser {
+	
+	/**
+	 * Test the parsing of invalid tablenames
+	 */
+	@Test
+	public void testTablenameParserInvalid() {
+		final List<String> invalidNames = new ArrayList<String>();
+		invalidNames.add("");
+		invalidNames.add(null);
+		invalidNames.add("abc");
+		invalidNames.add("abcd_abcd_abce");
+		invalidNames.add("__");
+		invalidNames.add("3__");
+		invalidNames.add("3_abc_");
+		invalidNames.add("3_abc_def_gef");
+		invalidNames.add("__def");
+
+		for(final String invalidTablename : invalidNames) {
+			final Tablename tablename = new Tablename(invalidTablename);
+			Assert.assertFalse(tablename.isValid());
+			Assert.assertEquals(Tablename.INVALID_GROUP, tablename.getGroup());
+			Assert.assertEquals(Tablename.INVALID_DIMENSION, tablename.getDimension());
+			Assert.assertEquals(Tablename.INVALID_IDENTIFIER, tablename.getIdentifier());
+		}
+		
+	}
+	
+	/**
+	 * Test the parsing of valid tablenames
+	 */
+	@Test
+	public void testTablenameParserValid() {
+		final List<String> validNames = new ArrayList<String>();
+		validNames.add("3_abc_def");
+		validNames.add("15_34_34");
+		validNames.add("122_def_34");
+		validNames.add("122_def_table21");
+		validNames.add("122_12def_table21");
+		
+		for(final String validTablename : validNames) {
+			final Tablename tablename = new Tablename(validTablename);
+			Assert.assertTrue(tablename.isValid());
+			Assert.assertNotNull(tablename.getGroup());
+			Assert.assertNotNull(tablename.getDimension());
+			Assert.assertNotNull(tablename.getIdentifier());
+		}
+	}
+}
