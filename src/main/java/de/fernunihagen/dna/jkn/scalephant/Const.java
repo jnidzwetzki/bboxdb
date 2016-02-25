aad0b6d7ab39d8b446254e0d793860a89a9c0e35
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-02-25 12:13:04 +0100

    Added the name of the config file to the const class

--- src/main/java/de/fernunihagen/dna/jkn/scalephant/Const.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/Const.java
@@ -2,7 +2,14 @@ package de.fernunihagen.dna.jkn.scalephant;
 
 public class Const {
 	
-	// The version of the software
+	/**
+	 *  The version of the software
+	 */
 	public final static String VERSION = "0.0.3";
+	
+	/**
+	 * The name of the configuration file
+	 */
+	public final static String CONFIG_FILE = "scalephant.yaml";
 	
 }
