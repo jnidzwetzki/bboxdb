8f0e7f01c92b40113d22e9839a2d11931b49037e
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-03-01 11:05:29 +0100

    Upgraded box search

--- src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/sstable/spatialindex/BoxSearchIndex.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/sstable/spatialindex/BoxSearchIndex.java
@@ -1,35 +1,46 @@
 package de.fernunihagen.dna.jkn.scalephant.storage.sstable.spatialindex;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 
+import de.fernunihagen.dna.jkn.scalephant.storage.entity.BoundingBox;
 import de.fernunihagen.dna.jkn.scalephant.storage.entity.Tuple;
 
 public class BoxSearchIndex implements SpatialIndexer {
-	
-	/**
-	 * An instance to generate random numbers
-	 */
-	protected final Random random = new Random();
-	
+
 	/**
 	 * The root element of the tree
 	 */
-	protected BoxNode rootElement;
-	
-	protected int getRandomPivotPosition(final int max) {
-		return random.nextInt() % max;
-	}
+	protected BoxNode rootElement;	
 	
 	public void constructFromTuples(final List<Tuple> tuples) {
 		rootElement = partition(tuples, 1);
 	}
 	
-	protected BoxNode partition(final List<Tuple> tuples, final int dimension) {
-		final int pivotPosition = getRandomPivotPosition(tuples.size());
-		final Tuple pivotTuple = tuples.get(pivotPosition);
+	/**
+	 * Annotate the tree with its min and max values
+	 * 
+	 * @param rootElement
+	 */
+	protected void annotateTree(final BoxNode rootElement) {
 		
+	}
+	
+	/**
+	 * Create a binary search tree from the tuple list
+	 * @param tuples
+	 * @param dimension
+	 * @return
+	 */
+	protected BoxNode partition(final List<Tuple> tuples, final int dimension) {
+	
+		sortTupleList(tuples);
+		
+		final int pivotPosition = tuples.size() / 2;
+		final Tuple pivotTuple = tuples.get(pivotPosition);
+				
 		final List<Tuple> smaller = new ArrayList<Tuple>();
 		final List<Tuple> bigger = new ArrayList<Tuple>();
 		
@@ -50,9 +61,33 @@ public class BoxSearchIndex implements SpatialIndexer {
 		subtreeRootnode.value = pivotTuple;
 		
 		final int dimensions = pivotTuple.getBoundingBox().getDimension();
-		subtreeRootnode.leftChild = partition(smaller, dimension + 1 % dimensions);
-		subtreeRootnode.rightChild = partition(bigger, dimension + 1 % dimensions);
+		
+		if(! smaller.isEmpty()) {
+			subtreeRootnode.leftChild = partition(smaller, dimension + 1 % dimensions);
+		} else {
+			subtreeRootnode.leftChild = null;
+		}
+		
+		if(! bigger.isEmpty()) {
+			subtreeRootnode.rightChild = partition(bigger, dimension + 1 % dimensions);
+		} else {
+			subtreeRootnode.rightChild = null;
+		}
+		
 		return subtreeRootnode;
+	}
+
+	/**
+	 * Sort the list of tuples, regarding the bounding box
+	 * @param tuples
+	 */
+	protected void sortTupleList(final List<Tuple> tuples) {
+		Collections.sort(tuples, new Comparator<Tuple>() {
+			@Override
+			public int compare(final Tuple tuple1, final Tuple tuple2) {
+				return tuple1.getBoundingBox().compareTo(tuple2.getBoundingBox());
+			}
+		});
 	}
 	
 }
@@ -61,4 +96,5 @@ class BoxNode {
 	protected BoxNode leftChild;
 	protected BoxNode rightChild;
 	protected Tuple value;
+	protected BoundingBox subtreeBoundingBox;
 }
