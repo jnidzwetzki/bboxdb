e7a0ab838686d8d7ced7adb712700ce4cad0a6e1
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-03-07 14:04:59 +0100

    Added the ability to manage multiple instances

--- src/main/java/de/fernunihagen/dna/jkn/scalephant/ScalephantMain.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/ScalephantMain.java
@@ -1,49 +1,71 @@
 package de.fernunihagen.dna.jkn.scalephant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.daemon.Daemon;
 import org.apache.commons.daemon.DaemonContext;
 import org.apache.commons.daemon.DaemonInitException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.fernunihagen.dna.jkn.scalephant.distribution.ZookeeperClient;
 import de.fernunihagen.dna.jkn.scalephant.network.server.ConnectionHandler;
 
 public class ScalephantMain implements Daemon {
 
 	/**
+	 * The instances to manage
+	 */
+	protected final List<Lifecycle> services = new ArrayList<Lifecycle>();
+	
+	/**
 	 * The Logger
 	 */
 	private final static Logger logger = LoggerFactory.getLogger(ScalephantMain.class);
-	
-	/**
-	 * The network connection handler
-	 */
-	protected ConnectionHandler connectionHandler = null;
 
 	@Override
 	public void init(final DaemonContext ctx) throws DaemonInitException, Exception {
 		logger.info("Init the scalephant");
-		connectionHandler = new ConnectionHandler();
+		
+		final ScalephantConfiguration scalephantConfiguration = ScalephantConfigurationManager.getConfiguration();
+		
+		// The zookeeper client
+		services.add(new ZookeeperClient(scalephantConfiguration.getZookeepernodes()));
+
+		// The network conenction handler
+		services.add(new ConnectionHandler());		
 	}
 
 	@Override
 	public void start() throws Exception {
-		logger.info("Starting up the scalephant - version: " + Const.VERSION);		
-		connectionHandler.init();
+		logger.info("Starting up the scalephant - version: " + Const.VERSION);	
+		
+		// Init all services
+		for(Lifecycle service : services) {
+			service.init();
+		}
 	}
 
 	@Override
 	public void stop() throws Exception {
 		logger.info("Stopping the scalephant");
-		connectionHandler.shutdown();
+		
+		// Stop all services
+		for(Lifecycle service : services) {
+			service.shutdown();
+		}
 	}
 	
 	@Override
 	public void destroy() {
 		logger.info("Destroy the scalephant");
-		connectionHandler = null;
+		services.clear();
 	}
 	
+	//===========================================
+	// Test method
+	//===========================================
 	public static void main(String[] args) throws DaemonInitException, Exception {
 		final ScalephantMain main = new ScalephantMain();
 		main.init(null);
