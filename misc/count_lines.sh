f7788ca0f2bdac04723fd5f257c9ac698591fe48
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-03-08 14:03:10 +0100

    Added yaml line count

--- misc/count_lines.sh
+++ misc/count_lines.sh
@@ -7,12 +7,14 @@
 loc_java=$( (find .. -name '*.java' -print0 | xargs -0 cat ) | wc -l)
 loc_shell=$( (find .. -name '*.sh' -print0 | xargs -0 cat ) | wc -l)
 loc_xml=$( (find .. -name '*.xml' -print0 | xargs -0 cat ) | wc -l)
+loc_yaml=$( (find .. -name '*.yaml' -print0 | xargs -0 cat ) | wc -l)
 
-loc=$((loc_java + $loc_shell + $loc_xml))
+loc=$((loc_java + $loc_shell + $loc_xml + $loc_yaml))
 
 printf "Lines of java code:\t %8d\n" $loc_java
 printf "Lines of shell code:\t %8d\n" $loc_shell
 printf "Lines of xml code:\t %8d\n" $loc_xml
+printf "Lines of yaml code:\t %8d\n" $loc_yaml
 printf "==================================\n"
 printf "Total lines of code:\t %8d\n" $loc
 printf "==================================\n"
