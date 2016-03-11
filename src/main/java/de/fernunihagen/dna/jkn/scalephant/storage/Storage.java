50cd6d0eda569ce72d9f5e2a1bbaaabd09abb1bd
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-03-11 13:25:59 +0100

    Added some comments

--- src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/Storage.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/storage/Storage.java
@@ -7,14 +7,40 @@ import de.fernunihagen.dna.jkn.scalephant.storage.entity.Tuple;
 
 public interface Storage {
 
+	/**
+	 * Store a tuple
+	 * @param tuple
+	 * @throws StorageManagerException
+	 */
 	public void put(final Tuple tuple) throws StorageManagerException;
 
+	/**
+	 * Search for tuple and return the most recent version
+	 * @param key
+	 * @return
+	 * @throws StorageManagerException
+	 */
 	public Tuple get(final String key) throws StorageManagerException;
 	
+	/**
+	 * Search retuns all tuples that are inside the query box
+	 * @param boundingBox
+	 * @return
+	 * @throws StorageManagerException
+	 */
 	public Collection<Tuple> getTuplesInside(final BoundingBox boundingBox) throws StorageManagerException;
 	
+	/**
+	 * Delete a tuple
+	 * @param key
+	 * @throws StorageManagerException
+	 */
 	public void delete(final String key) throws StorageManagerException;
 	
+	/**
+	 * Truncate the stored data
+	 * @throws StorageManagerException
+	 */
 	public void clear() throws StorageManagerException;
 
 }
