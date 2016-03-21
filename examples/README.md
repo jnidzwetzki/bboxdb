cfb188f53aea9a311b53aa5ea3707ae070b0beb3
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-03-21 11:36:53 +0100

    Added the first benchmark

--- examples/README.md
+++ examples/README.md
@@ -1,6 +1,7 @@
-# How to build an execute the examples
+# Info
+This directory contains some examples for the Scalephant. In addition, this document covers the tools to benchmark the software. To work with the examples and the benchmarks, the Scalephant has to be started first. This is shown in the next section.
 
-## Start a least one scalephant instance
+## Start one Scalephant instance or a Scalephant cluster
 
     # Local instance
     ./misc/manage_instance.sh zookeeper_start
@@ -11,7 +12,10 @@ or
     # Cluster instance
     ./misc/manage_cluster.sh zookeeper_start
     ./misc/manage_cluster.sh scalephant_start
-    
+
+# Examples
+The "client example" (ScalephantClientExample.java) shows how data can be inserted and queried.
+
 ## Build and execute the client example
 
 	# Build the example
@@ -19,3 +23,11 @@ or
 	# Execute the example
 	java -classpath "../target/*":"../target/lib/*":"../conf":"." ScalephantClientExample
 	
+# Benchmarking
+
+## The insert benchmark
+	
+	# Execute the benchmark
+	java -classpath "../target/*":"../target/lib/*":"../conf" de.fernunihagen.dna.jkn.scalephant.performance.BenchmarkInsertPerformance
+	
+## The key query benchmark
---
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/performance/BenchmarkInsertPerformance.java
@@ -0,0 +1,54 @@
+package de.fernunihagen.dna.jkn.scalephant.performance;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import de.fernunihagen.dna.jkn.scalephant.network.client.ScalephantClient;
+import de.fernunihagen.dna.jkn.scalephant.storage.entity.BoundingBox;
+import de.fernunihagen.dna.jkn.scalephant.storage.entity.Tuple;
+
+
+public class BenchmarkInsertPerformance {
+
+	
+	public static void main(String[] args) {
+		// A 2 dimensional table (member of distribution group 'mygroup3') with the name 'testdata'
+		final String mytable = "2_mygroup3_testdata";
+		
+		// Number of tuples
+		final int tuples = 1000000;
+		
+		final AtomicInteger insertedTuples = new AtomicInteger(0);
+		
+		// Connect to the server
+		final ScalephantClient scalephantClient = new ScalephantClient("127.0.0.1");
+		
+		if(! scalephantClient.isConnected()) {
+			System.out.println("Connection could not be established");
+			System.exit(-1);
+		}
+		
+		// Dump performance info every second
+		final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(10);
+		executorService.scheduleAtFixedRate(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println(insertedTuples.get());
+			}
+			
+		}, 0, 1, TimeUnit.SECONDS);
+	
+		// Insert the tuples
+		for(; insertedTuples.get() < tuples; insertedTuples.incrementAndGet()) {
+			scalephantClient.insertTuple(mytable, new Tuple(Integer.toString(insertedTuples.get()), BoundingBox.EMPTY_BOX, "abcdef".getBytes()));
+		}
+		
+		executorService.shutdown();
+		
+		System.out.println("Done");
+	}
+	
+}
