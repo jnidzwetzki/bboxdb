9fff70b64e29d6a34e01aedc3f2c5b6a0759b00d
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-03-22 12:42:56 +0100

    Refactored benchmark class

---
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/performance/AbstractBenchmark.java
@@ -0,0 +1,124 @@
+package de.fernunihagen.dna.jkn.scalephant.performance;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.fernunihagen.dna.jkn.scalephant.network.client.ScalephantClient;
+
+public abstract class AbstractBenchmark implements Runnable {
+	
+	/**
+	 * Unix time of the benchmark start
+	 */
+	protected long startTime = 0;
+	
+	/**
+	 * The scalephant client
+	 */
+	protected ScalephantClient scalephantClient;
+	
+	/**
+	 * The executor service
+	 */
+	protected ScheduledExecutorService executorService;
+	
+	/**
+	 * Status variable - is the benchmark actvive
+	 */
+	protected volatile boolean benchmarkActive;
+	
+	/**
+	 * The Logger
+	 */
+	private final static Logger logger = LoggerFactory.getLogger(AbstractBenchmark.class);
+
+
+	/**
+	 * Run the benchmark
+	 */
+	@Override
+	public void run() {
+		try {
+			prepare();
+			runBenchmark();
+			done();
+		} catch (Exception e) {
+			logger.error("Got an exception", e);
+			System.exit(-1);
+		}
+	}
+
+	/**
+	 * The default prepare method
+	 */
+	protected void prepare() throws Exception {
+		// Set the benchmark flag to active
+		benchmarkActive = true;
+		
+		// Connect to the scalephant server
+		scalephantClient = new ScalephantClient("127.0.0.1");
+		scalephantClient.connect();
+		
+		if(! scalephantClient.isConnected()) {
+			throw new Exception("Connection could not be established");
+		}
+		
+		executorService = Executors.newScheduledThreadPool(10);
+		
+		// Init the data table
+		final DataTable dataTable = getDataTable();
+		System.out.println(dataTable.getTableHeader());
+		
+		// Set the benchmark time
+		startTime = System.currentTimeMillis();
+		
+		// Dump performance info every second
+		executorService.scheduleAtFixedRate(new Runnable() {
+			
+			@Override
+			public void run() {
+				final StringBuffer sb = new StringBuffer();
+				sb.append(System.currentTimeMillis() - startTime + "\t");
+				
+				for(short i = 0; i < dataTable.getColumns(); i++) {
+					sb.append(dataTable.getValueForColum(i));
+					sb.append("\t");
+				}
+				
+				System.out.println(sb.toString());
+			}
+			
+		}, 0, 1, TimeUnit.SECONDS);
+	}
+	
+	/**
+	 * The benchmark
+	 */
+	protected abstract void runBenchmark() throws Exception;
+	
+	/**
+	 * The default done method
+	 */
+	protected void done() throws Exception {
+		
+		// Disconnect from server and shutdown the statistics thread
+		scalephantClient.disconnect();
+		executorService.shutdown();
+			
+		System.out.println("Done in " + (System.currentTimeMillis() - startTime) + " ms");
+		
+		// Set the benchmark flag to finish
+		benchmarkActive = false;
+	}
+	
+	/**
+	 * Get the data table for the print thread
+	 * @return
+	 */
+	protected abstract DataTable getDataTable();
+	
+}
--- src/main/java/de/fernunihagen/dna/jkn/scalephant/performance/BenchmarkInsertPerformance.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/performance/BenchmarkInsertPerformance.java
@@ -1,71 +1,83 @@
 package de.fernunihagen.dna.jkn.scalephant.performance;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import de.fernunihagen.dna.jkn.scalephant.network.client.ClientOperationFuture;
-import de.fernunihagen.dna.jkn.scalephant.network.client.ScalephantClient;
 import de.fernunihagen.dna.jkn.scalephant.storage.entity.BoundingBox;
 import de.fernunihagen.dna.jkn.scalephant.storage.entity.Tuple;
 
-
-public class BenchmarkInsertPerformance {
+public class BenchmarkInsertPerformance extends AbstractBenchmark {
 
 	/**
-	 * Unix time of the benchmark start
+	 * The amount of inserted tuples
 	 */
-	protected static long startTime = 0;
-	
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
+	protected AtomicInteger insertedTuples = new AtomicInteger(0);
+
+	@Override
+	public void runBenchmark() throws InterruptedException, ExecutionException {
 		// A 2 dimensional table (member of distribution group 'mygroup3') with the name 'testdata'
 		final String mytable = "2_mygroup3_testdata";
 		
 		// Number of tuples
 		final int tuples = 5000000;
-		
-		final AtomicInteger insertedTuples = new AtomicInteger(0);
-		
-		// Connect to the server
-		final ScalephantClient scalephantClient = new ScalephantClient("127.0.0.1");
-		scalephantClient.connect();
-		
-		if(! scalephantClient.isConnected()) {
-			System.out.println("Connection could not be established");
-			System.exit(-1);
-		}
-		
+				
 		// Remove old data
 		final ClientOperationFuture result = scalephantClient.deleteTable(mytable);
-		result.get();
-		
-		// Start benchmark
-		startTime = System.currentTimeMillis();
-		System.out.println("#Time\tTuples");
-		
-		// Dump performance info every second
-		final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(10);
-		executorService.scheduleAtFixedRate(new Runnable() {
-			
-			@Override
-			public void run() {
-				System.out.println(System.currentTimeMillis() - startTime + "\t" + insertedTuples.get());
-			}
-			
-		}, 0, 1, TimeUnit.SECONDS);
+		result.get();		
 	
 		// Insert the tuples
 		for(; insertedTuples.get() < tuples; insertedTuples.incrementAndGet()) {
 			scalephantClient.insertTuple(mytable, new Tuple(Integer.toString(insertedTuples.get()), BoundingBox.EMPTY_BOX, "abcdef".getBytes()));
 		}
-		
-		// Disconnect from server and shutdown the statistics thread
-		scalephantClient.disconnect();
-		executorService.shutdown();
+	}
+
+	@Override
+	protected DataTable getDataTable() {
+		return new DataTable() {
 			
-		System.out.println("Done in " + (System.currentTimeMillis() - startTime) + " ms");
+			protected int lastInsertedTuples = 0;
+			protected int diff = 0;
+			
+			@Override
+			public String getValueForColum(short colum) {
+				switch (colum) {
+				
+				// Total amount of inserted tuples
+				case 0:
+					final int tuples = insertedTuples.get();
+					diff = tuples - lastInsertedTuples;
+					lastInsertedTuples = tuples;
+					return Integer.toString(tuples);
+					
+				// Diff amount of inserted tuples
+				case 1:
+					return Integer.toString(diff);
+
+				default:
+					return "-----";
+				}
+			}
+			
+			@Override
+			public String getTableHeader() {
+				return "#Time\tTuples\tTuples_per_sec";
+			}
+			
+			@Override
+			public short getColumns() {
+				return 2;
+			}
+		};
+	}	
+	
+	/* ====================================================
+	 * Main
+	 * ====================================================
+	 */
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		final BenchmarkInsertPerformance benchmarkInsertPerformance = new BenchmarkInsertPerformance();
+		benchmarkInsertPerformance.run();
 	}
 	
 }
