d5b802ea26b3f2ffce3fafda2d701430479f0f95
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-04-25 21:20:32 +0200

    Added the gui helper

---
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/tools/gui/GuiHelper.java
@@ -0,0 +1,19 @@
+package de.fernunihagen.dna.jkn.scalephant.tools.gui;
+
+import java.awt.Dimension;
+import java.awt.Frame;
+
+public class GuiHelper {
+	
+	/**
+     * Set the position of the frame to the center of the screen
+     * 
+     */
+    public static void setCenterPosition(final Frame frame) {
+        Dimension paneSize   = frame.getSize();
+        Dimension screenSize = frame.getToolkit().getScreenSize();
+        frame.setLocation(
+            (screenSize.width  - paneSize.width)  / 2,
+            (screenSize.height - paneSize.height) / 2);
+    }
+}
