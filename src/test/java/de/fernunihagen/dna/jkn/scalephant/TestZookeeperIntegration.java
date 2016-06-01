5fb6d4d2deb5c6357ac4f5d3984b00419a4890b1
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-06-01 13:18:19 +0200

    Made Zookeeper integration test more verbose

--- src/test/java/de/fernunihagen/dna/jkn/scalephant/TestZookeeperIntegration.java
+++ src/test/java/de/fernunihagen/dna/jkn/scalephant/TestZookeeperIntegration.java
@@ -1,9 +1,12 @@
 package de.fernunihagen.dna.jkn.scalephant;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import de.fernunihagen.dna.jkn.scalephant.distribution.ZookeeperClient;
@@ -11,6 +14,30 @@ import de.fernunihagen.dna.jkn.scalephant.distribution.ZookeeperException;
 
 public class TestZookeeperIntegration {
 
+	/**
+	 * The zookeeper client
+	 */
+	protected  ZookeeperClient zookeeperClient;
+	
+	@Before
+	public void before() {
+		final ScalephantConfiguration scalephantConfiguration 
+			= ScalephantConfigurationManager.getConfiguration();
+	
+		final Collection<String> zookeepernodes = scalephantConfiguration.getZookeepernodes();
+		final String clustername = scalephantConfiguration.getClustername();
+
+		System.out.println("Zookeeper nodes are: " + zookeepernodes);
+		System.out.println("Zookeeper cluster is: " + clustername);
+	
+		zookeeperClient = new ZookeeperClient(zookeepernodes, clustername);
+		zookeeperClient.init();
+	}
+	
+	@After
+	public void after() {
+		zookeeperClient.shutdown();
+	}
 
 	/**
 	 * Test the id generation
@@ -18,16 +45,6 @@ public class TestZookeeperIntegration {
 	 */
 	@Test
 	public void testTableIdGenerator() throws ZookeeperException {
-		
-		final ScalephantConfiguration scalephantConfiguration 
-		     = ScalephantConfigurationManager.getConfiguration();
-		
-		final ZookeeperClient zookeeperClient 
-		     = new ZookeeperClient(scalephantConfiguration.getZookeepernodes(), 
-				                   scalephantConfiguration.getClustername());
-		
-		zookeeperClient.init();
-		
 		final List<Integer> ids = new ArrayList<Integer>();
 		
 		for(int i = 0; i < 10; i++) {
@@ -38,6 +55,5 @@ public class TestZookeeperIntegration {
 			ids.add(nextId);
 		}
 		
-		zookeeperClient.shutdown();
 	}
 }
