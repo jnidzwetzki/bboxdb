3dd70e85b27837f799b73a3c78ea2c05f422adc2
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-06-02 11:09:34 +0200

    Added two integration tests

--- src/test/java/de/fernunihagen/dna/jkn/scalephant/TestZookeeperIntegration.java
+++ src/test/java/de/fernunihagen/dna/jkn/scalephant/TestZookeeperIntegration.java
@@ -9,6 +9,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.fernunihagen.dna.jkn.scalephant.distribution.DistributionGroupName;
 import de.fernunihagen.dna.jkn.scalephant.distribution.ZookeeperClient;
 import de.fernunihagen.dna.jkn.scalephant.distribution.ZookeeperException;
 
@@ -54,6 +55,52 @@ public class TestZookeeperIntegration {
 			Assert.assertFalse(ids.contains(nextId));
 			ids.add(nextId);
 		}
+	}
+	
+	/**
+	 * Test the creation and the deletion of a distributon group
+	 * @throws ZookeeperException
+	 */
+	@Test
+	public void testDistributionGroupCreateDelete() throws ZookeeperException {
+		final String testGroup = "4_abc";
 		
+		// Create new group
+		zookeeperClient.deleteDistributionGroup(testGroup);
+		zookeeperClient.createDistributionGroup(testGroup, (short) 3); 
+		final List<DistributionGroupName> groups = zookeeperClient.getDistributionGroups();
+		System.out.println(groups);
+		boolean found = false;
+		for(final DistributionGroupName group : groups) {
+			if(group.getFullname().equals(testGroup)) {
+				found = true;
+			}
+		}
+		
+		Assert.assertTrue(found);
+		
+		// Delete group
+		zookeeperClient.deleteDistributionGroup(testGroup);
+		final List<DistributionGroupName> groups2 = zookeeperClient.getDistributionGroups();
+		found = false;
+		for(final DistributionGroupName group : groups2) {
+			if(group.getFullname().equals(testGroup)) {
+				found = true;
+			}
+		}
+		
+		Assert.assertFalse(found);
+	}
+	
+	/**
+	 * Test the replication factor of a distribution group
+	 * @throws ZookeeperException 
+	 */
+	@Test
+	public void testDistributionGroupReplicationFactor() throws ZookeeperException {
+		final String testGroup = "4_abc";
+		zookeeperClient.deleteDistributionGroup(testGroup);
+		zookeeperClient.createDistributionGroup(testGroup, (short) 3); 
+		Assert.assertEquals(3, zookeeperClient.getReplicationFactorForDistributionGroup(testGroup));
 	}
 }
