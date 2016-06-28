ac1960e5c125c8b9f71be00ee31024aae97664a4
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-06-28 14:13:41 +0200

    Added methods to split a sstable

--- src/main/java/de/fernunihagen/dna/jkn/scalephant/distribution/sstable/SSTableSplitter.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/distribution/sstable/SSTableSplitter.java
@@ -1,5 +1,20 @@
 package de.fernunihagen.dna.jkn.scalephant.distribution.sstable;
 
-public interface SSTableSplitter {
+import de.fernunihagen.dna.jkn.scalephant.storage.entity.SSTableName;
 
+public interface SSTableSplitter {
+	
+	/**
+	 * Is a split needed?
+	 * @param totalTuplesInTable
+	 * @return
+	 */
+	public boolean isSplitNeeded(final int totalTuplesInTable);
+
+	/**
+	 * Perform a SSTable split
+	 * @param ssTableName
+	 */
+	public void performSplit(final SSTableName ssTableName);
+	
 }
--- src/main/java/de/fernunihagen/dna/jkn/scalephant/distribution/sstable/SimpleSplitStrategy.java
+++ src/main/java/de/fernunihagen/dna/jkn/scalephant/distribution/sstable/SimpleSplitStrategy.java
@@ -1,6 +1,33 @@
 package de.fernunihagen.dna.jkn.scalephant.distribution.sstable;
 
+import de.fernunihagen.dna.jkn.scalephant.ScalephantConfiguration;
+import de.fernunihagen.dna.jkn.scalephant.ScalephantConfigurationManager;
+import de.fernunihagen.dna.jkn.scalephant.storage.entity.SSTableName;
+
 public class SimpleSplitStrategy implements SSTableSplitter  {
+
+	/**
+	 * Test if a split is needed
+	 */
+	@Override
+	public boolean isSplitNeeded(final int totalTuplesInTable) {
+		final ScalephantConfiguration configuration = ScalephantConfigurationManager.getConfiguration();
+		final int maxEntries = configuration.getSstableMaxEntries();
+		
+		if(totalTuplesInTable > maxEntries) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Perform a split of the given sstable
+	 */
+	@Override
+	public void performSplit(final SSTableName ssTableName) {
+		
+	}
 
 
 }
