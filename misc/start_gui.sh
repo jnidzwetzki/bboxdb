42fa71bc480d756cc524dcb0733673b823dbcd5e
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-06-29 12:57:45 +0200

    Added start gui script

---
+++ misc/start_gui.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Start the GUI of the scalephant 
+#
+#
+#########################################
+
+# Scriptname and Path 
+pushd `dirname $0` > /dev/null
+basedir=`pwd`
+fullpath=$(basename $0)
+popd > /dev/null
+
+cd ..
+mvn package -DskipTests
+cd $basedir
+
+# Find all jars
+cd $basedir
+
+# Does a build exists? 
+if [ -d ../target ]; then
+   libs=$(find ../target/lib -name '*.jar' | xargs echo | tr ' ' ':')
+   jar=$(ls -1 ../target/scalephant*.jar | tail -1)
+fi 
+
+classpath="$basedir/../conf:$libs:$jar"
+
+echo "Start the GUI"
+
+java -cp $classpath de.fernunihagen.dna.jkn.scalephant.tools.gui.Main
+
+exit 0
