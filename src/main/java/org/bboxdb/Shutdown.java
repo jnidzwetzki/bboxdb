676dc683d0d5471799adb99ae7b606bb2eed415f
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-12-20 16:05:35 +0100

    Reformat source

--- src/main/java/org/bboxdb/Shutdown.java
+++ src/main/java/org/bboxdb/Shutdown.java
@@ -34,6 +34,7 @@ public class Shutdown {
 
 	/**
 	 * Call the JMX service to shutdown the application
+	 * 
 	 * @param args
 	 */
 	public static void main(final String[] args) throws Exception {
@@ -42,28 +43,30 @@ public class Shutdown {
 			System.err.println("Usage: Shutdown <Port> <Password>");
 			System.exit(-1);
 		}
-		
+
 		final int jmxPort = Integer.parseInt(args[0]);
 		final String password = args[1];
-		
+
 		// Set username and password
 		final Map<String, String[]> env = new HashMap<>();
-		final String[] credentials = {"controlRoleUser", password};
+		final String[] credentials = { "controlRoleUser", password };
 		env.put(JMXConnector.CREDENTIALS, credentials);
-		        
-	    // Connect to JMX
-        System.out.println("Connecting to JMX shtutdown service.");
-        final JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://:" + jmxPort + "/jmxrmi");
-        
-        final JMXConnector jmxc = JMXConnectorFactory.connect(url, env);
-        final MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
 
-        final ObjectName mbeanName = new ObjectName(JMXService.MBEAN_LIFECYCLE);
-        final LifecycleMBean mbeanProxy = JMX.newMBeanProxy(mbsc, mbeanName, LifecycleMBean.class, true);
+		// Connect to JMX
+		System.out.println("Connecting to JMX shtutdown service.");
+		final JMXServiceURL url = new JMXServiceURL(
+				"service:jmx:rmi:///jndi/rmi://:" + jmxPort + "/jmxrmi");
 
-        mbeanProxy.shutdown();
+		final JMXConnector jmxc = JMXConnectorFactory.connect(url, env);
+		final MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
 
-        jmxc.close();
-        System.out.println("Done.");
+		final ObjectName mbeanName = new ObjectName(JMXService.MBEAN_LIFECYCLE);
+		final LifecycleMBean mbeanProxy = JMX.newMBeanProxy(mbsc, mbeanName,
+				LifecycleMBean.class, true);
+
+		mbeanProxy.shutdown();
+
+		jmxc.close();
+		System.out.println("Done.");
 	}
 }
