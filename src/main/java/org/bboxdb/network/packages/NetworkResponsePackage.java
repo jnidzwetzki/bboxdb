88c86e3f0014a5cd272c0820c871bd79c1ee166d
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-01-14 16:52:07 +0100

    Removed unused equals / hashCode

--- src/main/java/org/bboxdb/network/packages/NetworkResponsePackage.java
+++ src/main/java/org/bboxdb/network/packages/NetworkResponsePackage.java
@@ -29,27 +29,5 @@ public abstract class NetworkResponsePackage extends NetworkPackage {
 	 * @throws PackageEncodeError 
 	 */
 	public abstract byte[] getByteArray() throws PackageEncodeError;
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + sequenceNumber;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		NetworkResponsePackage other = (NetworkResponsePackage) obj;
-		if (sequenceNumber != other.sequenceNumber)
-			return false;
-		return true;
-	}
 	
 }
