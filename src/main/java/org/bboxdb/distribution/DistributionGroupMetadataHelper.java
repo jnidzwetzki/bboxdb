a75ec12a44d76afa7b0f32435ebcfa42bb1650c4
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-02-05 13:15:19 +0100

    Added meta data writer

---
+++ src/main/java/org/bboxdb/distribution/DistributionGroupMetadataHelper.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ *
+ *    Copyright (C) 2015-2017 the BBoxDB project
+ *  
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License. 
+ *    
+ *******************************************************************************/
+package org.bboxdb.distribution;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bboxdb.BBoxDBConfiguration;
+import org.bboxdb.BBoxDBConfigurationManager;
+import org.bboxdb.storage.entity.DistributionGroupMetadata;
+import org.bboxdb.storage.sstable.SSTableHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+public class DistributionGroupMetadataHelper {
+	
+	/**
+	 * The Logger
+	 */
+	private final static Logger logger = LoggerFactory.getLogger(DistributionGroupMetadataHelper.class);
+	
+	/**
+	 * Write new metadata for a distribution group
+	 * @param distributionGroupName
+	 * @param distributionGroupMetadata
+	 * @throws IOException 
+	 */
+	public static void writeMedatadataForGroup(final DistributionGroupName distributionGroupName, 
+			final DistributionGroupMetadata distributionGroupMetadata) throws IOException {
+		
+	    final Map<String, Object> data = new HashMap<>();
+	    data.put("version", distributionGroupMetadata.getVersion());
+	    
+	    final String filename = getFilename(distributionGroupName);
+	    final FileWriter writer = new FileWriter(new File(filename));
+	    logger.debug("Output data to: " + filename);
+	    
+	    final Yaml yaml = new Yaml();
+	    yaml.dump(data, writer);
+	    writer.close();
+	}
+
+	/**
+	 * Get the filename of the meta data
+	 * @param distributionGroupName
+	 * @return
+	 */
+	protected static String getFilename(final DistributionGroupName distributionGroupName) {
+		BBoxDBConfiguration bBoxDBConfiguration = BBoxDBConfigurationManager.getConfiguration();
+	    
+	    return SSTableHelper.getDistributionGroupMedatadaFile(bBoxDBConfiguration.getDataDirectory(), distributionGroupName.getFullname());
+	}
+
+	/**
+	 * Read the metadata data of a distribution group
+	 * @param distributionGroupName
+	 * @return
+	 */
+	public static DistributionGroupMetadata getMedatadaForGroup
+		(final DistributionGroupName distributionGroupName) {
+		
+		final Yaml yaml = new Yaml(); 
+	    final String filename = getFilename(distributionGroupName);
+
+		FileReader reader;
+		try {
+			reader = new FileReader(filename);
+		} catch (FileNotFoundException e) {
+			logger.warn("Unable to load file: " + filename, e);
+			return null;
+		}
+
+		return yaml.loadAs(reader, DistributionGroupMetadata.class);
+	}
+}
---
+++ src/test/java/org/bboxdb/TestDistributionGroupMedadata.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ *
+ *    Copyright (C) 2015-2017 the BBoxDB project
+ *  
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License. 
+ *    
+ *******************************************************************************/
+package org.bboxdb;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bboxdb.distribution.DistributionGroupMetadataHelper;
+import org.bboxdb.distribution.DistributionGroupName;
+import org.bboxdb.storage.entity.DistributionGroupMetadata;
+import org.bboxdb.storage.sstable.SSTableHelper;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestDistributionGroupMedadata {
+
+	/**
+	 * The distribution group name for the tests
+	 */
+	protected final static DistributionGroupName DGROUP_NAME = new DistributionGroupName("2_testgroup");
+	
+	/**
+	 * The Logger
+	 */
+	private final static Logger logger = LoggerFactory.getLogger(TestDistributionGroupMedadata.class);
+	
+	
+	/**
+	 * Create the needed directory
+	 */
+	@BeforeClass
+	public static void beforeClass() {
+		BBoxDBConfiguration bBoxDBConfiguration = BBoxDBConfigurationManager.getConfiguration();
+	    
+	    final String dirName = SSTableHelper.getDistributionGroupDir(bBoxDBConfiguration.getDataDirectory(), DGROUP_NAME.getFullname());
+
+	    logger.info("Create: {}", dirName);
+	    
+	    final File dir = new File(dirName);
+	    dir.mkdir();
+	}
+	
+	/**
+	 * Delete the needed directory
+	 */
+	@AfterClass
+	public static void afterClass() {
+		BBoxDBConfiguration bBoxDBConfiguration = BBoxDBConfigurationManager.getConfiguration();
+	    
+	    final String dirName = SSTableHelper.getDistributionGroupDir(bBoxDBConfiguration.getDataDirectory(), DGROUP_NAME.getFullname());
+	 
+	    logger.info("Remove: {}", dirName);
+
+	    final File dir = new File(dirName);
+	    dir.delete();
+	}
+	
+	/**
+	 * Test reading and writing distribution group meta data
+	 * @throws IOException 
+	 */
+	@Test
+	public void testReadAndWriteDistributionGroupMetadata() throws IOException {
+		final DistributionGroupMetadata distributionGroupMetadata1 = new DistributionGroupMetadata();
+		final DistributionGroupMetadata distributionGroupMetadata2 = new DistributionGroupMetadata();
+		distributionGroupMetadata1.setVersion(10);
+		distributionGroupMetadata2.setVersion(20);
+		Assert.assertFalse(distributionGroupMetadata1.equals(distributionGroupMetadata2));
+		
+		DistributionGroupMetadataHelper.writeMedatadataForGroup(DGROUP_NAME, distributionGroupMetadata1);
+		final DistributionGroupMetadata distributionGroupMetadata1read = DistributionGroupMetadataHelper.getMedatadaForGroup(DGROUP_NAME);
+		
+		DistributionGroupMetadataHelper.writeMedatadataForGroup(DGROUP_NAME, distributionGroupMetadata2);
+		final DistributionGroupMetadata distributionGroupMetadata2read = DistributionGroupMetadataHelper.getMedatadaForGroup(DGROUP_NAME);
+		
+		Assert.assertTrue(distributionGroupMetadata1read.equals(distributionGroupMetadata1));
+		Assert.assertTrue(distributionGroupMetadata2read.equals(distributionGroupMetadata2));
+	}
+
+}
