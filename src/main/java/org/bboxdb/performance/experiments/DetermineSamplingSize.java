df7142f5ed8f99026ebd568ace2b66752ff51c69
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-03-23 13:30:10 +0100

    Added more sampling sizes

--- src/main/java/org/bboxdb/performance/experiments/DetermineSamplingSize.java
+++ src/main/java/org/bboxdb/performance/experiments/DetermineSamplingSize.java
@@ -106,6 +106,8 @@ public class DetermineSamplingSize implements Runnable, OSMStructureCallback {
 		System.out.format("Imported %d objects\n", numberOfElements);
 		
 		final List<Double> sampleSizes = Arrays.asList(
+				0.01d, 0.02d, 0.03d, 0.04d, 0.05d,
+				0.06d, 0.07d, 0.08d, 0.09d,
 				0.1d, 0.2d, 0.3d, 0.4d, 0.5d, 
 				0.6d, 0.7d, 0.8d, 0.9d, 1.0d, 
 				5d, 10d, 20d, 30d, 40d, 50d, 60d);
