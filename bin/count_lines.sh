67c4d30519ccabaf9d8a87f4cebc5b5b0e07559c
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-05-24 07:38:36 +0200

    Count python lines

--- bin/count_lines.sh
+++ bin/count_lines.sh
@@ -12,14 +12,16 @@ fi
 # Lines of code
 loc_java=$( (find $BBOXDB_HOME -name '*.java' -print0 | xargs -0 cat ) | wc -l)
 loc_shell=$( (find $BBOXDB_HOME -name '*.sh' -print0 | xargs -0 cat ) | wc -l)
+loc_python=$( (find $BBOXDB_HOME -name '*.py' -print0 | xargs -0 cat ) | wc -l)
 loc_xml=$( (find $BBOXDB_HOME -name '*.xml' -print0 | xargs -0 cat ) | wc -l)
 loc_yaml=$( (find $BBOXDB_HOME -name '*.yaml' -print0 | xargs -0 cat ) | wc -l)
 loc_markdown=$( (find $BBOXDB_HOME -name '*.md' -print0 | xargs -0 cat ) | wc -l)
 
-loc=$(($loc_java + $loc_shell + $loc_xml + $loc_yaml + $loc_markdown))
+loc=$(($loc_java + $loc_shell + $loc_python + $loc_xml + $loc_yaml + $loc_markdown))
 
 printf "Lines of java code:\t %8d\n" $loc_java
 printf "Lines of shell code:\t %8d\n" $loc_shell
+printf "Lines of python code:\t %8d\n" $loc_python
 printf "Lines of xml code:\t %8d\n" $loc_xml
 printf "Lines of yaml code:\t %8d\n" $loc_yaml
 printf "Lines of markdown code:\t %8d\n" $loc_markdown
