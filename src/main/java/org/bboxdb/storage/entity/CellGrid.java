e6485cf371f46b3b0139d5c9818e3ece828c9d25
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-05-27 13:52:49 +0200

    Changed interface to native java

--- src/main/java/org/bboxdb/storage/entity/CellGrid.java
+++ src/main/java/org/bboxdb/storage/entity/CellGrid.java
@@ -23,9 +23,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 
 public class CellGrid {
