2045ce27cb2029200b11cb4bf8665b0f24c9756e
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-06-19 23:40:43 +0200

    Increased timeout

--- src/main/java/org/bboxdb/distribution/zookeeper/ZookeeperClient.java
+++ src/main/java/org/bboxdb/distribution/zookeeper/ZookeeperClient.java
@@ -78,7 +78,7 @@ public class ZookeeperClient implements BBoxDBService, Watcher {
 	/**
 	 * The timeout for the zookeeper session in miliseconds
 	 */
-	protected final static int ZOOKEEPER_SESSION_TIMEOUT = (int) TimeUnit.SECONDS.toMillis(20);
+	protected final static int ZOOKEEPER_SESSION_TIMEOUT = (int) TimeUnit.SECONDS.toMillis(30);
 
 	/**
 	 * The connect timeout in seconds
