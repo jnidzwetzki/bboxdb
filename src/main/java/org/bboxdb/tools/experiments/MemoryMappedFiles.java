aa6585f742628da020340757acdf3361c323a65f
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-06-20 21:02:35 +0200

    Added further tests

--- src/main/java/org/bboxdb/tools/experiments/MemoryMappedFiles.java
+++ src/main/java/org/bboxdb/tools/experiments/MemoryMappedFiles.java
@@ -17,7 +17,11 @@
  *******************************************************************************/
 package org.bboxdb.tools.experiments;
 
+import java.io.File;
+import java.io.RandomAccessFile;
 import java.lang.management.ManagementFactory;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 import javax.management.AttributeNotFoundException;
 import javax.management.InstanceNotFoundException;
@@ -27,59 +31,93 @@ import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 import javax.management.ReflectionException;
 
+import org.bboxdb.util.io.UnsafeMemoryHelper;
+
 /**
- * Test the behavior of the VM memory when
- * memory mapped files are used
+ * Test the behavior of the VM memory when memory mapped files are used
  *
  */
 public class MemoryMappedFiles {
-	
+
+	/**
+	 * The size of one MB
+	 */
+	public static final int MB = 1024*1024*1024;
+
 	/**
 	 * Print the JVM memory statistics
 	 */
 	protected static void printMemoryStatistics() {
 		final long maxMemory = Runtime.getRuntime().maxMemory();
-		System.out.println("Maximum memory (bytes): " + 
-				  (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
+		System.out.println("Maximum memory (bytes): " + (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
 
-		System.out.println("Total memory (bytes): " + 
-		Runtime.getRuntime().totalMemory());
-		  
-		System.out.println("Free memory within total (bytes): " + 
-		Runtime.getRuntime().freeMemory());
+		System.out.println("Total memory (bytes): " + Runtime.getRuntime().totalMemory());
+
+		System.out.println("Free memory within total (bytes): " + Runtime.getRuntime().freeMemory());
 	}
-	
+
 	/**
 	 * Print the memory mapped MBean data
+	 * 
 	 * @param args
-	 * @throws MalformedObjectNameException 
-	 * @throws MBeanException 
-	 * @throws ReflectionException 
-	 * @throws AttributeNotFoundException 
-	 * @throws InstanceNotFoundException 
+	 * @throws MalformedObjectNameException
+	 * @throws MBeanException
+	 * @throws ReflectionException
+	 * @throws AttributeNotFoundException
+	 * @throws InstanceNotFoundException
 	 */
-	protected static void printMappedStatistics() throws MalformedObjectNameException, 
-			InstanceNotFoundException, AttributeNotFoundException, ReflectionException, MBeanException {
-		
+	protected static void printMappedStatistics() throws MalformedObjectNameException, InstanceNotFoundException,
+			AttributeNotFoundException, ReflectionException, MBeanException {
+
 		final ObjectName objectName = new ObjectName("java.nio:type=BufferPool,name=mapped");
 		final MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
 		final Long mmapCount = (Long) mbeanServer.getAttribute(objectName, "Count");
-	    final Long mmapMemoryUsed = (Long) mbeanServer.getAttribute(objectName, "MemoryUsed");
+		final Long mmapMemoryUsed = (Long) mbeanServer.getAttribute(objectName, "MemoryUsed");
 
-	    System.out.format("Number of mmaps %d number of mmap memory %d", 
-	    		mmapCount.longValue(), mmapMemoryUsed.longValue());
+		System.out.format("Number of mmaps %d number of mmap memory %d\n", mmapCount.longValue(),
+				mmapMemoryUsed.longValue());
+		
+		final boolean unmapperState = UnsafeMemoryHelper.isDirectMemoryUnmapperAvailable();
+		
+		System.out.println("The memory mapped unmapper is available: " + unmapperState);
 	}
 
 	/**
 	 * Main * main * main * main * main * main
+	 * 
 	 * @param args
-	 * @throws MBeanException 
-	 * @throws ReflectionException 
-	 * @throws AttributeNotFoundException 
-	 * @throws InstanceNotFoundException 
-	 * @throws MalformedObjectNameException 
+	 * @throws MBeanException
+	 * @throws ReflectionException
+	 * @throws AttributeNotFoundException
+	 * @throws InstanceNotFoundException
+	 * @throws MalformedObjectNameException
 	 */
 	public static void main(final String[] args) throws Exception {
+		printMemoryStatistics();
+		printMappedStatistics();
+
+		final File fileOne = File.createTempFile("mmap1", ".bin");
+		System.out.println("Mapping file: " + fileOne);
+		fileOne.deleteOnExit();
+
+		final RandomAccessFile randomAccessFileOne = new RandomAccessFile(fileOne, "rw");
+		randomAccessFileOne.setLength(100 * MB);
+		
+		final FileChannel fileChannel = randomAccessFileOne.getChannel();
+		final long size = fileChannel.size();
+		MappedByteBuffer memory = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, size);
+		
+		System.out.println("Mapped file with size: " + size);
+		
+		printMemoryStatistics();
+		printMappedStatistics();
+
+		System.out.println("Unmapping file");
+		fileChannel.close();
+		randomAccessFileOne.close();
+		UnsafeMemoryHelper.unmapMemory(memory);
+		memory = null;
+		
 		printMemoryStatistics();
 		printMappedStatistics();
 	}
