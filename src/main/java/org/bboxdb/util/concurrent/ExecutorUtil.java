32598c22a1a76927881f5986396cc45201db9445
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-06-27 12:55:29 +0200

    max threads /2 as initial size

--- src/main/java/org/bboxdb/util/concurrent/ExecutorUtil.java
+++ src/main/java/org/bboxdb/util/concurrent/ExecutorUtil.java
@@ -40,7 +40,7 @@ public class ExecutorUtil {
 		
 		// The Pool executor executes up to maxThreads in parallel. If the queue is full, the caller
 		// has to execute the task directly. 
-		return new ThreadPoolExecutor(maxThreads, maxThreads, 30, TimeUnit.SECONDS, 
+		return new ThreadPoolExecutor(maxThreads / 2, maxThreads, 30, TimeUnit.SECONDS, 
 				linkedBlockingDeque, new ThreadPoolExecutor.CallerRunsPolicy());
 	}
 
