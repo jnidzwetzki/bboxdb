ec8001a96d83f4b5f24e48c6338a733cc1ea3881
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-07-10 07:01:00 +0200

    Added system info class

---
+++ src/main/java/org/bboxdb/util/SystemInfo.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ *
+ *    Copyright (C) 2015-2017 the BBoxDB project
+ *  
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License. 
+ *    
+ *******************************************************************************/
+package org.bboxdb.util;
+
+import java.io.File;
+
+public class SystemInfo {
+
+	/**
+	 * Get the amount of CPU cores in this system
+	 * @return
+	 */
+	public static int getCPUCores() {
+		return Runtime.getRuntime().availableProcessors();
+	}
+	
+	/**
+	 * Get the amount of available memory for this JVM
+	 * @return
+	 */
+	public static long getAvailableMemory() {
+		return Runtime.getRuntime().totalMemory();
+	}
+	
+	/**
+	 * Get the total diskspace on the location
+	 * @param path
+	 * @return
+	 */
+	public static long getTotalDiskspace(final File path) {
+		return path.getTotalSpace();
+	}
+	
+	/**
+	 * Get the free diskspace on the location
+	 * @param location
+	 * @return
+	 */
+	public static long getFreeDiskspace(final File path) {
+		return path.getUsableSpace();
+	}
+
+}
