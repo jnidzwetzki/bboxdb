d15655cab884c5bf39e83e69c61fe527358d349a
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-08-25 13:13:38 +0200

    Improved closeable util

--- src/main/java/org/bboxdb/util/CloseableHelper.java
+++ src/main/java/org/bboxdb/util/CloseableHelper.java
@@ -19,14 +19,29 @@ package org.bboxdb.util;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.function.Consumer;
 
 public class CloseableHelper {
 
+	/**
+	 * A consumer that prints the exception on stderr
+	 */
+	public static final Consumer<Exception> PRINT_EXCEPTION_ON_STDERR = 
+				(e) -> System.err.println("Exception while closing: " + e);
+	
 	/**
 	 * Close the closeable without throwing an exception or logging
 	 * @param closeable
 	 */
 	public static void closeWithoutException(final Closeable closeable) {
+		closeWithoutException(closeable, null);
+	}
+	
+	/**
+	 * Close the closeable without throwing an exception or logging
+	 * @param closeable
+	 */
+	public static void closeWithoutException(final Closeable closeable, final Consumer<Exception> consumer) {
 		if(closeable == null) {
 			return;
 		}
@@ -34,7 +49,35 @@ public class CloseableHelper {
 		try {
 			closeable.close();
 		} catch (IOException e) {
-			// Ignore Exception
+			if(consumer != null) {
+				consumer.accept(e);
+			}
+		}
+	}
+	
+	/**
+	 * Close the closeable without throwing an exception or logging
+	 * @param closeable
+	 */
+	public static void closeWithoutException(final AutoCloseable closeable) {
+		closeWithoutException(closeable, null);
+	}
+	
+	/**
+	 * Close the closeable without throwing an exception or logging
+	 * @param closeable
+	 */
+	public static void closeWithoutException(final AutoCloseable closeable, final Consumer<Exception> consumer) {
+		if(closeable == null) {
+			return;
+		}
+		
+		try {
+			closeable.close();
+		} catch (Exception e) {
+			if(consumer != null) {
+				consumer.accept(e);
+			}
 		}
 	}
 
