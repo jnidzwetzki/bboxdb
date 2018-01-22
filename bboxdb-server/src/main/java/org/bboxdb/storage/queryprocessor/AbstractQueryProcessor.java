ab6594d3bbc3450167f9dece1bd23b2ddae6bb99
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-01-22 12:38:34 +0100

    Introduced the abstract query processor

---
+++ bboxdb-server/src/main/java/org/bboxdb/storage/queryprocessor/AbstractQueryProcessor.java
@@ -0,0 +1,144 @@
+/*******************************************************************************
+ *
+ *    Copyright (C) 2015-2018 the BBoxDB project
+ *  
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License. 
+ *    
+ *******************************************************************************/
+package org.bboxdb.storage.queryprocessor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.bboxdb.commons.DuplicateResolver;
+import org.bboxdb.storage.StorageManagerException;
+import org.bboxdb.storage.entity.Tuple;
+import org.bboxdb.storage.entity.TupleStoreConfiguration;
+import org.bboxdb.storage.sstable.duplicateresolver.TupleDuplicateResolverFactory;
+import org.bboxdb.storage.tuplestore.ReadOnlyTupleStore;
+import org.bboxdb.storage.tuplestore.manager.TupleStoreManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbstractQueryProcessor {
+	
+	/**
+	 * The sstable manager
+	 */
+	protected final TupleStoreManager tupleStoreManager;
+
+	/**
+	 * Is the iterator ready?
+	 */
+	protected boolean ready;
+	
+	/**
+	 * The seen tuples<Key, Timestamp> map
+	 */
+	protected final Map<String, Long> seenTuples;
+	
+	/**
+	 * The unprocessed storages
+	 */
+	protected final List<ReadOnlyTupleStore> unprocessedStorages;
+
+	/**
+	 * The aquired storages
+	 */
+	protected final List<ReadOnlyTupleStore> aquiredStorages;
+
+	/**
+	 * The Logger
+	 */
+	protected static final Logger logger = LoggerFactory.getLogger(QueryProcessor.class);
+	
+	public AbstractQueryProcessor(final TupleStoreManager tupleStoreManager) {
+		this.tupleStoreManager = tupleStoreManager;
+		this.ready = false;
+		this.seenTuples = new HashMap<String, Long>();
+		this.aquiredStorages = new LinkedList<ReadOnlyTupleStore>();
+		this.unprocessedStorages = new LinkedList<ReadOnlyTupleStore>();
+	}
+
+	/**
+	 * Cleanup all aquired tables
+	 */
+	protected void cleanup() {
+		ready = false;
+		tupleStoreManager.releaseStorage(aquiredStorages);
+		aquiredStorages.clear();
+		unprocessedStorages.clear();
+	}
+
+	/**
+	 * Prepare the unprocessed storage list
+	 * @throws StorageManagerException 
+	 */
+	protected void prepareUnprocessedStorage() {
+		
+		try {
+			aquiredStorages.clear();
+			aquiredStorages.addAll(tupleStoreManager.aquireStorage());
+			
+			unprocessedStorages.clear();
+			unprocessedStorages.addAll(aquiredStorages);
+	
+			// Sort tables regarding the newest tuple timestamp 
+			// The newest storage should be on top of the list
+			unprocessedStorages.sort((storage1, storage2) 
+					-> Long.compare(storage2.getNewestTupleVersionTimestamp(), 
+							        storage1.getNewestTupleVersionTimestamp()));
+			
+			ready = true;
+		
+		} catch (StorageManagerException e) {
+			logger.error("Unable to aquire tables", e);
+			cleanup();
+		}
+	}
+
+	/**
+	 * Get the most recent version of the tuple
+	 * e.g. Memtables can contain multiple versions of the key
+	 * The iterator can return an outdated version
+	 * 
+	 * @param tuple
+	 * @return
+	 * @throws StorageManagerException 
+	 */
+	protected List<Tuple> getVersionsForTuple(final Tuple tuple, final ReadOnlyTupleStore activeStorage) throws StorageManagerException {
+		
+		final List<Tuple> resultTuples = new ArrayList<>();
+		resultTuples.addAll(activeStorage.get(tuple.getKey()));
+		
+		for(final ReadOnlyTupleStore readOnlyTupleStorage : unprocessedStorages) {
+			final List<Tuple> possibleTuples = readOnlyTupleStorage.get(tuple.getKey());
+			resultTuples.addAll(possibleTuples);
+		}
+		
+		final TupleStoreConfiguration tupleStoreConfiguration 
+			= tupleStoreManager.getTupleStoreConfiguration();
+		
+		final DuplicateResolver<Tuple> resolver 
+			= TupleDuplicateResolverFactory.build(tupleStoreConfiguration);
+		
+		// Removed unwanted tuples for key
+		resolver.removeDuplicates(resultTuples);
+		
+		return resultTuples;
+	}
+
+}
--- bboxdb-server/src/main/java/org/bboxdb/storage/queryprocessor/QueryProcessor.java
+++ bboxdb-server/src/main/java/org/bboxdb/storage/queryprocessor/QueryProcessor.java
@@ -18,70 +18,26 @@
 package org.bboxdb.storage.queryprocessor;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
-import org.bboxdb.commons.DuplicateResolver;
 import org.bboxdb.storage.StorageManagerException;
 import org.bboxdb.storage.entity.Tuple;
-import org.bboxdb.storage.entity.TupleStoreConfiguration;
 import org.bboxdb.storage.queryprocessor.queryplan.QueryPlan;
-import org.bboxdb.storage.sstable.duplicateresolver.TupleDuplicateResolverFactory;
 import org.bboxdb.storage.tuplestore.ReadOnlyTupleStore;
 import org.bboxdb.storage.tuplestore.manager.TupleStoreManager;
 import org.bboxdb.storage.util.CloseableIterator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class QueryProcessor {
+public class QueryProcessor extends AbstractQueryProcessor {
 
 	/**
 	 * The query plan to evaluate
 	 */
 	protected final QueryPlan queryplan;
 	
-	/**
-	 * The sstable manager
-	 */
-	protected final TupleStoreManager tupleStoreManager;
-
-	/**
-	 * Is the iterator ready?
-	 */
-	protected boolean ready;
-	
-	/**
-	 * The seen tuples<Key, Timestamp> map
-	 */
-	protected final Map<String, Long> seenTuples;
-	
-	/**
-	 * The unprocessed storages
-	 */
-	protected final List<ReadOnlyTupleStore> unprocessedStorages;
-
-	/**
-	 * The aquired storages
-	 */
-	protected final List<ReadOnlyTupleStore> aquiredStorages;
-
-	
-	/**
-	 * The Logger
-	 */
-	protected static final Logger logger = LoggerFactory.getLogger(QueryProcessor.class);
-	
-	
 	public QueryProcessor(final QueryPlan queryplan, final TupleStoreManager ssTableManager) {
+		super(ssTableManager);
 		this.queryplan = queryplan;
-		this.tupleStoreManager = ssTableManager;
-		this.ready = false;
-		this.seenTuples = new HashMap<String, Long>();
-		this.aquiredStorages = new LinkedList<ReadOnlyTupleStore>();
-		this.unprocessedStorages = new LinkedList<ReadOnlyTupleStore>();
 	}
 	
 	public CloseableIterator<Tuple> iterator() {
@@ -153,49 +109,22 @@ public class QueryProcessor {
 				
 					if(seenTuples.containsKey(possibleTuple.getKey())) {
 						final long oldTimestamp = seenTuples.get(possibleTuple.getKey());
-						if(oldTimestamp < possibleTuple.getVersionTimestamp()) {
-							logger.warn("Unprocessded: {}", unprocessedStorages);
-							logger.warn("Aquired: {}", aquiredStorages);
-							logger.warn("Got newer tuple {} than {}", possibleTuple, oldTimestamp);
+						
+						if(oldTimestamp < possibleTuple.getVersionTimestamp()) {	
+							logger.error("This sould not happen! Got newer tuple later in "
+									+ "query processing. Unprocessed storages {}, aquired storage "
+									+ "{}, new tuple {}, old timestamp", unprocessedStorages, 
+									aquiredStorages, possibleTuple, oldTimestamp);
+							
 							seenTuples.put(possibleTuple.getKey(), possibleTuple.getVersionTimestamp());
 						}
 					} else {
 						// Set nextTuple != null to exit the loop
-						nextTuples.addAll(getVersionsForTuple(possibleTuple));
+						final List<Tuple> tupleVersions = getVersionsForTuple(possibleTuple, activeStorage);
+						nextTuples.addAll(tupleVersions);
 						seenTuples.put(possibleTuple.getKey(), possibleTuple.getVersionTimestamp());
 					}
 				}
-			}
-			
-			/**
-			 * Get the most recent version of the tuple
-			 * e.g. Memtables can contain multiple versions of the key
-		 	 * The iterator can return an outdated version
-		 	 * 
-			 * @param tuple
-			 * @return
-			 * @throws StorageManagerException 
-			 */
-			public List<Tuple> getVersionsForTuple(final Tuple tuple) throws StorageManagerException {
-				
-				final List<Tuple> resultTuples = new ArrayList<>();
-				resultTuples.addAll(activeStorage.get(tuple.getKey()));
-				
-				for(final ReadOnlyTupleStore readOnlyTupleStorage : unprocessedStorages) {
-					final List<Tuple> possibleTuples = readOnlyTupleStorage.get(tuple.getKey());
-					resultTuples.addAll(possibleTuples);
-				}
-				
-				final TupleStoreConfiguration tupleStoreConfiguration 
-					= tupleStoreManager.getTupleStoreConfiguration();
-				
-				final DuplicateResolver<Tuple> resolver 
-					= TupleDuplicateResolverFactory.build(tupleStoreConfiguration);
-				
-				// Removed unwanted tuples for key
-				resolver.removeDuplicates(resultTuples);
-				
-				return resultTuples;
 			}
 			
 			@Override
@@ -232,42 +161,4 @@ public class QueryProcessor {
 			}
 		};
 	}
-	
-	/**
-	 * Cleanup all aquired tables
-	 */
-	protected void cleanup() {
-		ready = false;
-		tupleStoreManager.releaseStorage(aquiredStorages);
-		aquiredStorages.clear();
-		unprocessedStorages.clear();
-	}
-
-	/**
-	 * Prepare the unprocessed storage list
-	 * @throws StorageManagerException 
-	 */
-	protected void prepareUnprocessedStorage()  {
-		
-		try {
-			aquiredStorages.clear();
-			aquiredStorages.addAll(tupleStoreManager.aquireStorage());
-			
-			unprocessedStorages.clear();
-			unprocessedStorages.addAll(aquiredStorages);
-	
-			// Sort tables regarding the newest tuple timestamp 
-			// The newest storage should be on top of the list
-			unprocessedStorages.sort((storage1, storage2) 
-					-> Long.compare(storage2.getNewestTupleVersionTimestamp(), 
-							        storage1.getNewestTupleVersionTimestamp()));
-			
-			ready = true;
-		
-		} catch (StorageManagerException e) {
-			logger.error("Unable to aquire tables", e);
-			cleanup();
-		}
-	}
-	
 }
