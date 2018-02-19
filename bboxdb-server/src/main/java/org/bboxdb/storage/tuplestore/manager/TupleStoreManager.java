df5bea28d993c1b2b9396ad54617de3681d821cc
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-02-19 14:35:40 +0100

    Allow reinit

--- bboxdb-server/src/main/java/org/bboxdb/storage/tuplestore/manager/TupleStoreManager.java
+++ bboxdb-server/src/main/java/org/bboxdb/storage/tuplestore/manager/TupleStoreManager.java
@@ -136,6 +136,7 @@ public class TupleStoreManager implements BBoxDBService {
 	public void init() {
 
 		if(! (serviceState.isInNewState() || serviceState.isInFinishedState())) {
+			serviceState.reset();
 			logger.warn("SSTable manager state is not new init() is called: {}", serviceState.getState());
 			return;
 		}
