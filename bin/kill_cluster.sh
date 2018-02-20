c3b89a7433770b7dc1039d744d700571fceeb32a
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-02-20 09:37:18 +0100

    Adapted changes

--- bin/kill_cluster.sh
+++ bin/kill_cluster.sh
@@ -57,27 +57,27 @@ $BBOXDB_HOME/bin/manage_cluster.sh  bboxdb_start
 sleep 5
 
 if [[ $1 != "nopopulate" ]]; then
-   $BBOXDB_HOME/bin/cli.sh -action create_dgroup -dgroup testgroup -replicationfactor 1 -dimensions 2
-   $BBOXDB_HOME/bin/cli.sh -action create_table -table testgroup_table1
-   $BBOXDB_HOME/bin/cli.sh -action create_table -table testgroup_table2
+   $BBOXDB_HOME/bin/cli.sh -action create_dgroup -dgroup mydgroup -replicationfactor 1 -dimensions 2
+   $BBOXDB_HOME/bin/cli.sh -action create_table -table mydgroup_table1
+   $BBOXDB_HOME/bin/cli.sh -action create_table -table mydgroup_table2
 
-   # Build table 1
-   $BBOXDB_HOME/bin/cli.sh -action insert -table testgroup_table1 -key key1a -bbox 1:3,1:3 -value value1
-   $BBOXDB_HOME/bin/cli.sh -action insert -table testgroup_table1 -key key2a -bbox 5:8,5:8 -value value2
-   $BBOXDB_HOME/bin/cli.sh -action query -table testgroup_table1 -bbox 0:20:0:20
+   # Tuples of table1
+   $BBOXDB_HOME/bin/cli.sh -action insert -table mydgroup_table1 -key tuple_a -bbox 1:8,1:2 -value value_a
+   $BBOXDB_HOME/bin/cli.sh -action insert -table mydgroup_table1 -key tuple_b -bbox 4:6,0:5 -value value_b
+   $BBOXDB_HOME/bin/cli.sh -action insert -table mydgroup_table1 -key tuple_c -bbox 8:10,8:10 -value value_c
 
-   # Build table 2
-   $BBOXDB_HOME/bin/cli.sh -action insert -table testgroup_table2 -key key1b -bbox 1:2,1:2 -value value1
-   $BBOXDB_HOME/bin/cli.sh -action insert -table testgroup_table2 -key key2b -bbox 1:10,1:10 -value value2
-   $BBOXDB_HOME/bin/cli.sh -action query -table testgroup_table2 -bbox 0:20:0:20
+   # Tuples of table2
+   $BBOXDB_HOME/bin/cli.sh -action insert -table mydgroup_table2 -key tuple_1 -bbox 1:3,5:6 -value value_1
+   $BBOXDB_HOME/bin/cli.sh -action insert -table mydgroup_table2 -key tuple_2 -bbox 4.5:5.5,1.5:4.5 -value value_2
+   $BBOXDB_HOME/bin/cli.sh -action insert -table mydgroup_table2 -key tuple_3 -bbox 7.5:10,1.5:2.5 -value value_3
 
-   # Join
-   $BBOXDB_HOME/bin/cli.sh -action join -table testgroup_table1:testgroup_table2 -bbox 0:20:0:20
+   # Join 
+   $BBOXDB_HOME/bin/cli.sh -action join -table mydgroup_table1:mydgroup_table2 -bbox 0:10:0:8
 
    # Key query and delete
-   $BBOXDB_HOME/bin/cli.sh -action query -table testgroup_table2 -key key1b
-   $BBOXDB_HOME/bin/cli.sh -action delete -table testgroup_table2 -key key1b
-   $BBOXDB_HOME/bin/cli.sh -action query -table testgroup_table2 -key key1b
-   $BBOXDB_HOME/bin/cli.sh -action query -table testgroup_table2 -bbox 0:20:0:20
+   $BBOXDB_HOME/bin/cli.sh -action query -table mydgroup_table1 -key tuple_a
+   $BBOXDB_HOME/bin/cli.sh -action delete -table mydgroup_table1 -key tuple_a
+   $BBOXDB_HOME/bin/cli.sh -action query -table mydgroup_table1 -key tuple_a
+   $BBOXDB_HOME/bin/cli.sh -action query -table mydgroup_table1 -bbox 0:20:0:20
 fi
 
