dcbb0145e73d12d4f6c808c8f5506e9e36a2ea49
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-03-10 00:00:36 +0100

    Introduced the dummy resource placement strategy

---
+++ bboxdb-server/src/main/java/org/bboxdb/distribution/placement/DummyResourcePlacementStrategy.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ *
+ *    Copyright (C) 2015-2018 the BBoxDB project
+ *  
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License. 
+ *    
+ *******************************************************************************/
+package org.bboxdb.distribution.placement;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.bboxdb.distribution.membership.BBoxDBInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.annotations.VisibleForTesting;
+
+public class DummyResourcePlacementStrategy extends ResourcePlacementStrategy {
+
+	/**
+	 * The system
+	 */
+	public final static BBoxDBInstance DUMMY_INSTANCE = new BBoxDBInstance("10.10.10.10:50050");
+	/**
+	 * The Logger
+	 */
+	private final static Logger logger = LoggerFactory.getLogger(DummyResourcePlacementStrategy.class);
+	
+	
+
+	@Override
+	@VisibleForTesting
+	public BBoxDBInstance getInstancesForNewRessource(final List<BBoxDBInstance> systems, 
+			final Collection<BBoxDBInstance> blacklist) throws ResourceAllocationException {
+		
+		logger.info("Executing new dummy allocation to {}", DUMMY_INSTANCE);
+		
+		return DUMMY_INSTANCE;
+	}
+}
