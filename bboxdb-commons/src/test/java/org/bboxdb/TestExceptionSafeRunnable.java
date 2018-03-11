1eefbe231eae519ebc196467f110a257be853075
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-03-11 20:44:59 +0100

    Added test case

---
+++ bboxdb-commons/src/test/java/org/bboxdb/TestExceptionSafeRunnable.java
@@ -0,0 +1,110 @@
+package org.bboxdb;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.bboxdb.commons.concurrent.ExceptionSafeRunnable;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestExceptionSafeRunnable {
+	
+	/**
+	 * Test the runnable without an exception
+	 * @throws InterruptedException 
+	 */
+	@Test(timeout=10000)
+	public void testWithoutException() throws InterruptedException {
+		
+		final AtomicInteger runTheadCalls = new AtomicInteger(0);
+		final AtomicInteger beginHookCalls = new AtomicInteger(0);
+		final AtomicInteger endHookCalls = new AtomicInteger(0);
+		final AtomicInteger afterExceptionCalls = new AtomicInteger(0);
+		
+		/**
+		 * Because the methods are protected, we need a real mock here
+		 * Mockito can't verify protected method calls
+		 */
+		final ExceptionSafeRunnable runnable = new ExceptionSafeRunnable() {
+			
+			@Override
+			protected void runThread() throws Exception {
+				runTheadCalls.incrementAndGet();
+			}
+			
+			@Override
+			protected void beginHook() {
+				beginHookCalls.incrementAndGet();
+			}
+			
+			@Override
+			protected void endHook() {
+				endHookCalls.incrementAndGet();
+			}
+			
+			@Override
+			protected void afterExceptionHook() {
+				afterExceptionCalls.incrementAndGet();
+			}
+		};
+		
+		final Thread thread = new Thread(runnable);
+		thread.start();
+		thread.join();
+		
+		Assert.assertEquals(1, runTheadCalls.get());
+		Assert.assertEquals(1, beginHookCalls.get());
+		Assert.assertEquals(1, endHookCalls.get());
+		Assert.assertEquals(0, afterExceptionCalls.get());
+	}
+	
+	/**
+	 * Test the runnable with an exception
+	 * @throws InterruptedException 
+	 */
+	@Test(timeout=10000)
+	public void testWithException() throws InterruptedException {
+		final AtomicInteger runTheadCalls = new AtomicInteger(0);
+		final AtomicInteger beginHookCalls = new AtomicInteger(0);
+		final AtomicInteger endHookCalls = new AtomicInteger(0);
+		final AtomicInteger afterExceptionCalls = new AtomicInteger(0);
+		
+		/**
+		 * Because the methods are protected, we need a real mock here
+		 * Mockito can't verify protected method calls
+		 */
+		final ExceptionSafeRunnable runnable = new ExceptionSafeRunnable() {
+			
+			@Override
+			protected void runThread() throws Exception {
+				runTheadCalls.incrementAndGet();
+				throw new Exception("Exception");
+			}
+			
+			@Override
+			protected void beginHook() {
+				beginHookCalls.incrementAndGet();
+			}
+			
+			@Override
+			protected void endHook() {
+				endHookCalls.incrementAndGet();
+			}
+			
+			@Override
+			protected void afterExceptionHook() {
+				afterExceptionCalls.incrementAndGet();
+			}
+		};
+		
+		final Thread thread = new Thread(runnable);
+		thread.start();
+		thread.join();
+		
+		Assert.assertEquals(1, runTheadCalls.get());
+		Assert.assertEquals(1, beginHookCalls.get());
+		Assert.assertEquals(0, endHookCalls.get());
+		Assert.assertEquals(1, afterExceptionCalls.get());
+	}
+	
+}
