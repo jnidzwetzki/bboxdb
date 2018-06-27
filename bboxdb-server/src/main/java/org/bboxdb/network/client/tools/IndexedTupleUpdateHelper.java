1b24442d1b23bae2bc0ae55dd534e1b6df08ff7e
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-06-27 12:12:27 +0200

    Fixed typo

--- bboxdb-server/src/main/java/org/bboxdb/network/client/tools/IndexedTupleUpdateHelper.java
+++ bboxdb-server/src/main/java/org/bboxdb/network/client/tools/IndexedTupleUpdateHelper.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  *
  *    Copyright (C) 2015-2018 the BBoxDB project
- *  
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
- *    limitations under the License. 
- *    
+ *    limitations under the License.
+ *
  *******************************************************************************/
 package org.bboxdb.network.client.tools;
 
@@ -43,22 +43,22 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Lists;
 
 public class IndexedTupleUpdateHelper {
-	
+
 	/**
 	 * The reference to the BBoxDB cluster
 	 */
 	private final BBoxDBCluster cluster;
-	
+
 	/**
 	 * The future store
 	 */
 	private final FixedSizeFutureStore futureStore;
-	
+
 	/**
 	 * The suffix for the index table
 	 */
 	public final static String IDX_DGROUP_PREFIX ="#idx#";
-	
+
 	/**
 	 * The default replication factor
 	 */
@@ -68,67 +68,67 @@ public class IndexedTupleUpdateHelper {
 	 * The max number of retries
 	 */
 	public final static int TOTAL_RETRIES = 10;
-	
+
 	/**
 	 * The Logger
 	 */
 	private final static Logger logger = LoggerFactory.getLogger(IndexedTupleUpdateHelper.class);
-	
-	
+
+
 	public IndexedTupleUpdateHelper(final BBoxDBCluster cluster) {
 		this.cluster = cluster;
 		this.futureStore = new FixedSizeFutureStore(10);
-		
+
 		// Log failed futures
 		futureStore.addFailedFutureCallback(f -> logger.error("Failed future" + f.getAllMessages()));
 	}
-	
+
 	/**
 	 * Handle the tuple update (supported by indices)
 	 * @param deletedTuple
-	 * @return 
-	 * @throws BBoxDBException 
-	 * @throws InterruptedException 
+	 * @return
+	 * @throws BBoxDBException
+	 * @throws InterruptedException
 	 */
-	public EmptyResultFuture handleTupleUpdate(final String table, final Tuple tuple) 
+	public EmptyResultFuture handleTupleUpdate(final String table, final Tuple tuple)
 			throws BBoxDBException, InterruptedException {
-		
+
 		try {
 			// Might be full space covering box when index entry not found
 			final Hyperrectangle oldBoundingBox = getAndLockOldBundingBoxForTuple(table, tuple);
-			
+
 			// Delete old tuple
 			final String key = tuple.getKey();
 			final long deletionTimestamp = tuple.getVersionTimestamp() - 1;
 			final EmptyResultFuture deleteFuture = cluster.deleteTuple(
 					table, key, deletionTimestamp, oldBoundingBox);
 			futureStore.put(deleteFuture);
-			
+
 			// Insert new tuple
 			final EmptyResultFuture insertFuture = cluster.insertTuple(table, tuple);
 			futureStore.put(insertFuture);
 
 			// Update index (and remove index locks)
 			updateIndexEntry(table, tuple);
-			
+
 			return insertFuture;
 		} catch (ZookeeperException | ZookeeperNotFoundException e) {
 			throw new BBoxDBException(e);
-		} 
+		}
 	}
-	
+
 	/**
 	 * Update the bounding box index
 	 * @param tuple
-	 * @throws BBoxDBException 
+	 * @throws BBoxDBException
 	 */
 	private void updateIndexEntry(final String table, final Tuple tuple) throws BBoxDBException {
 		final String boundingBoxValue = tuple.getBoundingBox().toCompactString();
 		final String tablename = convertTablenameToIndexTablename(table);
-		
+
 		final String key = tuple.getKey();
 		final Hyperrectangle indexBox = getBoundingBoxForKey(key);
-		
+
 		final Tuple tupleToUpdate = new Tuple(key, indexBox, boundingBoxValue.getBytes());
 		final EmptyResultFuture insertFuture = cluster.insertTuple(tablename, tupleToUpdate);
 		futureStore.put(insertFuture);
@@ -148,17 +148,17 @@ public class IndexedTupleUpdateHelper {
 	 * Get the old bounding box for the tuple
 	 * @param tuple
 	 * @return
-	 * @throws ZookeeperNotFoundException 
-	 * @throws ZookeeperException 
-	 * @throws BBoxDBException 
-	 * @throws InterruptedException 
+	 * @throws ZookeeperNotFoundException
+	 * @throws ZookeeperException
+	 * @throws BBoxDBException
+	 * @throws InterruptedException
 	 */
-	private Hyperrectangle getAndLockOldBundingBoxForTuple(final String table, final Tuple tuple) 
+	private Hyperrectangle getAndLockOldBundingBoxForTuple(final String table, final Tuple tuple)
 			throws ZookeeperException, ZookeeperNotFoundException, BBoxDBException, InterruptedException {
-		
+
 		final String indexTableName = createMissingTables(table);
-		
-		return tryToGetIndexEntry(indexTableName, tuple);	
+
+		return tryToGetIndexEntry(indexTableName, tuple);
 	}
 
 	/**
@@ -172,12 +172,12 @@ public class IndexedTupleUpdateHelper {
 	@VisibleForTesting
 	public String createMissingTables(final String table)
 			throws ZookeeperException, ZookeeperNotFoundException, BBoxDBException, InterruptedException {
-		
+
 		final ZookeeperClient zookeeperClient = cluster.getZookeeperClient();
-		
+
 		final String indexTableName = convertTablenameToIndexTablename(table);
 		final TupleStoreName tupleStoreName = new TupleStoreName(indexTableName);
-		
+
 		createDistributionGroupIfMissing(zookeeperClient, tupleStoreName);
 		createTableIfMissing(zookeeperClient, indexTableName, tupleStoreName);
 		return indexTableName;
@@ -187,28 +187,28 @@ public class IndexedTupleUpdateHelper {
 	 * Get the old index entry
 	 * @param tuple
 	 * @param indexTableName
-	 * @return 
-	 * @throws InterruptedException 
-	 * @throws BBoxDBException 
+	 * @return
+	 * @throws InterruptedException
+	 * @throws BBoxDBException
 	 */
 	@VisibleForTesting
-	public Optional<Tuple> getOldIndexEntry(final String indexTableName, final String key) 
+	public Optional<Tuple> getOldIndexEntry(final String indexTableName, final String key)
 			throws InterruptedException, BBoxDBException {
-		
+
 		final Hyperrectangle boundingBox = getBoundingBoxForKey(key);
 		final TupleListFuture resultFuture = cluster.queryRectangle(indexTableName, boundingBox);
 		resultFuture.waitForCompletion();
-		
+
 		if(resultFuture.isFailed()) {
 			logger.error("Index query future failed {}", resultFuture.getAllMessages());
 			return Optional.empty();
 		} else {
 			final List<Tuple> resultList = Lists.newArrayList(resultFuture.iterator());
-			
+
 			final Optional<Tuple> indexEntry = resultList.stream()
 					.filter(t -> t.getKey().equals(key))
 					.findAny();
-								
+
 			if(indexEntry.isPresent()) {
 				return indexEntry;
 			} else {
@@ -218,46 +218,46 @@ public class IndexedTupleUpdateHelper {
 			}
 		}
 	}
-	
+
 	/**
 	 * Try to get the index entry
 	 * @param tuple
 	 * @param indexTableName
-	 * @return 
+	 * @return
 	 * @throws BBoxDBException
 	 * @throws InterruptedException
 	 */
 	private Hyperrectangle tryToGetIndexEntry(final String indexTableName, final Tuple tuple)
 			throws BBoxDBException, InterruptedException {
-				
+
 		for(int i = 0; i < TOTAL_RETRIES; i++) {
-			
+
 			final Optional<Tuple> oldIndexEntryOpt = getOldIndexEntry(indexTableName, tuple.getKey());
-			
+
 			if(! oldIndexEntryOpt.isPresent()) {
 				Thread.sleep(100);
 				continue;
 			}
-			
+
 			final Tuple oldIndexEntry = oldIndexEntryOpt.get();
-				
+
 			final EmptyResultFuture lockResult = cluster.lockTuple(indexTableName, oldIndexEntry, true);
 			lockResult.waitForCompletion();
-			
-			// Lock successfull
+
+			// Lock was successful
 			if(! lockResult.isFailed()) {
 				final byte[] boundingBoxData = oldIndexEntry.getDataBytes();
 				return new Hyperrectangle(new String(boundingBoxData));
 			}
 		}
-		
+
 		throw new BBoxDBException("Unable to lock index entry in " + TOTAL_RETRIES + " rounds");
 	}
 
 
 	/**
 	 * Create the index table if missing
-	 * 
+	 *
 	 * @param zookeeperClient
 	 * @param tablename
 	 * @param tupleStoreName
@@ -269,25 +269,25 @@ public class IndexedTupleUpdateHelper {
 	private void createTableIfMissing(final ZookeeperClient zookeeperClient, final String tablename,
 			final TupleStoreName tupleStoreName)
 			throws ZookeeperException, ZookeeperNotFoundException, BBoxDBException, InterruptedException {
-		
+
 		final String distributionGroup = tupleStoreName.getDistributionGroup();
 
 		final TupleStoreAdapter tupleStoreAdapter = new TupleStoreAdapter(zookeeperClient);
 		final List<String> allTables = tupleStoreAdapter.getAllTables(distributionGroup);
-		
+
 		if(allTables.contains(tablename)) {
 			return;
 		}
-		
+
 		logger.info("Table {} not found, creating", tablename);
 		final TupleStoreConfiguration tableconfig = TupleStoreConfigurationBuilder
 				.create()
 				.allowDuplicates(false)
 				.build();
-		
+
 		final EmptyResultFuture createResult = cluster.createTable(tablename, tableconfig);
 		createResult.waitForCompletion();
-		
+
 		if(createResult.isFailed()) {
 			throw new BBoxDBException("Got an exception while creating table " + createResult.getAllMessages());
 		}
@@ -295,7 +295,7 @@ public class IndexedTupleUpdateHelper {
 
 	/**
 	 * Create the distribution group if missing
-	 * 
+	 *
 	 * @param zookeeperClient
 	 * @param tupleStoreName
 	 * @throws ZookeeperException
@@ -306,26 +306,26 @@ public class IndexedTupleUpdateHelper {
 	private void createDistributionGroupIfMissing(final ZookeeperClient zookeeperClient,
 			final TupleStoreName tupleStoreName)
 			throws ZookeeperException, ZookeeperNotFoundException, BBoxDBException, InterruptedException {
-		
+
 		final String distributionGroup = tupleStoreName.getDistributionGroup();
-		
+
 		final DistributionGroupAdapter distributionGroupAdapter = new DistributionGroupAdapter(zookeeperClient);
-		
+
 		final List<String> allGroups = distributionGroupAdapter.getDistributionGroups();
-		
+
 		if(allGroups.contains(distributionGroup)) {
 			return;
 		}
-		
+
 		logger.info("Distribution group {} not found, creating", distributionGroup);
 		final DistributionGroupConfiguration dgroupConfig = DistributionGroupConfigurationBuilder
 				.create(1)
 				.withReplicationFactor(DEFAULT_REPLIATION_FACTOR)
 				.build();
-		
+
 		final EmptyResultFuture dgroupFuture = cluster.createDistributionGroup(distributionGroup, dgroupConfig);
 		dgroupFuture.waitForCompletion();
-		
+
 		if(dgroupFuture.isFailed()) {
 			throw new BBoxDBException("Unable to create distribution group: " + dgroupFuture.getAllMessages());
 		}
@@ -333,7 +333,7 @@ public class IndexedTupleUpdateHelper {
 
 	/**
 	 * Get the name of the index table
-	 * 
+	 *
 	 * @param table
 	 * @return
 	 */
@@ -341,7 +341,7 @@ public class IndexedTupleUpdateHelper {
 	public static String convertTablenameToIndexTablename(final String table) {
 		return IDX_DGROUP_PREFIX + table;
 	}
-	
+
 	/**
 	 * Wait for the completion of future operations
 	 * @throws InterruptedException
