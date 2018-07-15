07692c864587a6b6691f6e8730016006dad91c6d
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-07-15 08:39:25 +0200

    Removed async

--- bboxdb-tools/src/main/java/org/bboxdb/tools/TupleFileReader.java
+++ bboxdb-tools/src/main/java/org/bboxdb/tools/TupleFileReader.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  *
  *    Copyright (C) 2015-2018 the BBoxDB project
- *
+ *  
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- *
+ *  
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ *  
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *
+ *    limitations under the License. 
+ *    
  *******************************************************************************/
 package org.bboxdb.tools;
 
@@ -24,12 +24,9 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
-import org.bboxdb.commons.concurrent.ExecutorUtil;
 import org.bboxdb.storage.entity.Tuple;
 import org.bboxdb.tools.converter.tuple.TupleBuilder;
 import org.bboxdb.tools.converter.tuple.TupleBuilderFactory;
@@ -40,23 +37,23 @@ public class TupleFileReader {
 	 * The filename to read
 	 */
 	private String filename;
-
+	
 	/**
 	 * The input format
 	 */
 	private final TupleBuilder tupleBuilder;
-
+	
 	/**
 	 * The bounding box padding
 	 */
 	private final double boxPadding;
-
+	
 	/**
 	 * The tuple callbacks
 	 */
 	private final List<Consumer<? super Tuple>> callbacks;
 
-	/**
+	/** 
 	 * The amount of processed lines
 	 */
 	private long lineNumber;
@@ -65,7 +62,7 @@ public class TupleFileReader {
 	 * The last read file
 	 */
 	private String fileLine;
-
+	
 	/**
 	 * The default bounding box padding
 	 */
@@ -74,16 +71,16 @@ public class TupleFileReader {
 	public TupleFileReader(final String filename, final String importFormat) {
 		this(filename, importFormat, DEFAULT_BOX_PADDING);
 	}
-
+	
 	public TupleFileReader(final String filename, final String importFormat, final double boxPadding) {
 		this.filename = filename;
 		this.boxPadding = boxPadding;
 		this.tupleBuilder = TupleBuilderFactory.getBuilderForFormat(importFormat);
 		this.callbacks = new ArrayList<>();
-
+		
 		tupleBuilder.setPadding(boxPadding);
 	}
-
+	
 	/**
 	 * Add a listener for each processed tuple
 	 * @param listener
@@ -91,60 +88,45 @@ public class TupleFileReader {
 	public void addTupleListener(final Consumer<? super Tuple> listener) {
 		callbacks.add(listener);
 	}
-
+	
 	/**
 	 * Process all lines of the file
 	 * @throws IOException
-	 * @throws InterruptedException
 	 */
-	public void processFile() throws IOException, InterruptedException {
+	public void processFile() throws IOException {
 		processFile(Long.MAX_VALUE);
 	}
-
+	
 	/**
 	 * Read the file with a maximal number of lines
-	 * @throws IOException
-	 * @throws InterruptedException
+	 * @throws IOException 
 	 */
-	public void processFile(final long maxLines) throws IOException, InterruptedException {
-
-		// Executor with 1 thread and max 100 pending tasks
-		final ExecutorService executorService = ExecutorUtil.getBoundThreadPoolExecutor(1, 100);
-
+	public void processFile(final long maxLines) throws IOException {
 		final File file = new File(filename);
-
 		if(! file.exists()) {
 			throw new IOException("Unable to open file: " + file);
 		}
-
+		
 		try(final Stream<String> fileStream = Files.lines(Paths.get(filename))) {
 			lineNumber = 1;
-
+			
 			for (final Iterator<String> iterator = fileStream.iterator(); iterator.hasNext();) {
 				fileLine = iterator.next();
 				final Tuple tuple = tupleBuilder.buildTuple(Long.toString(lineNumber), fileLine);
-
+				
 				if(tuple != null) {
-
-					// Process callback async
-					callbacks.forEach(c -> executorService.submit(() -> {
-						c.accept(tuple);
-					}));
-
+					callbacks.forEach(c -> c.accept(tuple));
 				}
-
+				
 				lineNumber++;
-
+				
 				if(lineNumber > maxLines) {
 					break;
 				}
 			}
 		}
-
-		executorService.shutdown();
-		executorService.awaitTermination(1, TimeUnit.DAYS);
 	}
-
+	
 	/**
 	 * Get the amount of processed tuples
 	 * @return
@@ -152,7 +134,7 @@ public class TupleFileReader {
 	public long getProcessedLines() {
 		return lineNumber;
 	}
-
+	
 	/**
 	 * The last read line
 	 * @return
@@ -160,7 +142,7 @@ public class TupleFileReader {
 	public String getLastReadLine() {
 		return fileLine;
 	}
-
+	
 	/**
 	 * Get the bounding box padding
 	 * @return
