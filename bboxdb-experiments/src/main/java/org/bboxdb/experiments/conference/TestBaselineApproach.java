c1ff9637423a886629068ec8311a5141393aaebd
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-07-18 22:28:42 +0200

    Implemented join

--- bboxdb-experiments/src/main/java/org/bboxdb/experiments/conference/TestBaselineApproach.java
+++ bboxdb-experiments/src/main/java/org/bboxdb/experiments/conference/TestBaselineApproach.java
@@ -145,25 +145,86 @@ public class TestBaselineApproach implements AutoCloseable {
 	 * @param args
 	 */
 	public void executeJoin(final String args[]) {
-		if(args.length != 5) {
-			System.err.println("Usage: join <table1> <table2> <format> <range>");
+		if(args.length != 6) {
+			System.err.println("Usage: join <table1> <table2> <format1> <format2> <range>");
 			System.exit(-1);
 		}
 
 		final Hyperrectangle hyperrectangle = new Hyperrectangle(args[4]);
 
-		executeJoin(args[1], args[2], args[3], hyperrectangle);
+		executeJoin(args[1], args[2], args[3], args[4], hyperrectangle);
 	}
 
 	/**
-	 * Execute a join
+	 * Execute a join (nested loop join)
 	 * @param table1
 	 * @param table2
 	 * @param range
 	 */
-	public void executeJoin(final String table1, final String table2, final String format,
-			final Hyperrectangle range) {
+	public void executeJoin(final String table1, final String table2, final String format1,
+			final String format2, final Hyperrectangle range) {
 
+		System.out.println("# Execute join query in range " + range);
+
+		final Stopwatch stopwatch = Stopwatch.createStarted();
+
+		final TupleBuilder tupleBuilder1 = TupleBuilderFactory.getBuilderForFormat(format1);
+	    final TupleBuilder tupleBuilder2 = TupleBuilderFactory.getBuilderForFormat(format2);
+
+		long readRecords = 0;
+		long resultRecords = 0;
+
+		final SimpleStatement statementTable1 = new SimpleStatement("SELECT * FROM " + table1);
+		statementTable1.setFetchSize(2000); // Use 2000 tuples per page
+
+		final ResultSet rs1 = session.execute(statementTable1);
+
+		for (final Row row1 : rs1) {
+
+			// Request next page
+		    if (rs1.getAvailableWithoutFetching() == 100 && !rs1.isFullyFetched()) {
+		        rs1.fetchMoreResults(); // this is asynchronous
+		    }
+
+		    readRecords++;
+
+		    final long id1 = row1.getLong(0);
+		    final String text1 = row1.getString(1);
+
+		    final Tuple tuple1 = tupleBuilder1.buildTuple(Long.toString(id1), text1);
+
+		    // Tuple is outside of our query range
+		    if(! tuple1.getBoundingBox().intersects(range)) {
+		    	continue;
+		    }
+
+			// Perform the nested loop join
+		    final SimpleStatement statementTable2 = new SimpleStatement("SELECT * FROM " + table2);
+			statementTable2.setFetchSize(2000); // Use 2000 tuples per page
+
+			final ResultSet rs2 = session.execute(statementTable2);
+
+			for (final Row row2 : rs2) {
+				// Request next page
+			    if (rs1.getAvailableWithoutFetching() == 100 && !rs1.isFullyFetched()) {
+			        rs1.fetchMoreResults(); // this is asynchronous
+			    }
+
+			    readRecords++;
+
+			    final long id2 = row2.getLong(0);
+			    final String text2 = row2.getString(1);
+
+			    final Tuple tuple2 = tupleBuilder2.buildTuple(Long.toString(id2), text2);
+
+			    if(tuple1.getBoundingBox().intersects(tuple2.getBoundingBox())) {
+			    	resultRecords++;
+			    }
+			}
+		}
+
+		System.out.println("# Read records " + readRecords + " result records " + resultRecords);
+		System.out.println("# Execution time in sec " + stopwatch.elapsed(TimeUnit.SECONDS));
 	}
 
 
