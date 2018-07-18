89fd2f96d87b5bb734ff7b6d2287149d38461fc0
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-07-18 12:03:41 +0200

    Ensure enough bytes are read

--- bboxdb-server/src/main/java/org/bboxdb/storage/wal/WriteAheadLogReader.java
+++ bboxdb-server/src/main/java/org/bboxdb/storage/wal/WriteAheadLogReader.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  *
  *    Copyright (C) 2015-2018 the BBoxDB project
- *  
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
- *    limitations under the License. 
- *    
+ *    limitations under the License.
+ *
  *******************************************************************************/
 package org.bboxdb.storage.wal;
 
@@ -32,14 +32,16 @@ import org.bboxdb.storage.util.TupleHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.io.ByteStreams;
+
 public class WriteAheadLogReader implements Closeable, Iterable<Tuple> {
-	
+
 	/**
 	 * The input stream
 	 */
 	private BufferedInputStream inputStream;
 
-	/** 
+	/**
 	 * The file
 	 */
 	private final File file;
@@ -48,34 +50,34 @@ public class WriteAheadLogReader implements Closeable, Iterable<Tuple> {
 	 * The Logger
 	 */
 	private static final Logger logger = LoggerFactory.getLogger(WriteAheadLogReader.class);
-	
-	/** 
+
+	/**
 	 * The tuple iterator
 	 */
 	private final class TupleIterator implements Iterator<Tuple> {
-		
+
 		/**
 		 * The next tuple
 		 */
 		private Tuple nextTuple = null;
-		
+
 		@Override
 		public boolean hasNext() {
-			
+
 			// We have already fetched a tuple
 			if(nextTuple != null) {
 				return true;
 			}
-			
+
 			try {
 				final int availableBytes = inputStream.available();
-				
+
 				if(availableBytes == 0) {
 					return false;
 				}
-				
+
 				nextTuple = TupleHelper.decodeTuple(inputStream);
-				
+
 				return true;
 			} catch (IOException e) {
 				logger.error("Got IO exception", e);
@@ -85,36 +87,36 @@ public class WriteAheadLogReader implements Closeable, Iterable<Tuple> {
 
 		@Override
 		public Tuple next() {
-			
+
 			if(nextTuple == null) {
 				throw new RuntimeException("next() called but nextTupe is null,"
 						+ " do you forget to call hasNext()?");
 			}
-			
+
 			final Tuple tupleToReturn = nextTuple;
 			nextTuple = null;
 			return tupleToReturn;
 		}
 	}
-	
+
 	public WriteAheadLogReader(final File basedir, final int memtableNumber) throws IOException, StorageManagerException {
 		this(WriteAheadLogManager.getFileForWal(basedir, memtableNumber));
 	}
-	
+
 	public WriteAheadLogReader(final File file) throws IOException, StorageManagerException {
-		
+
 		if(! file.exists()) {
 			throw new IOException("File " + file + " does not exist");
 		}
-		
+
 		this.file = file;
 		this.inputStream = new BufferedInputStream(new FileInputStream(file));
-				
+
 		// Validate file - read the magic from the beginning
 		final byte[] expectedMagic = SSTableConst.MAGIC_BYTES_WAL;
 		final byte[] magicBytes = new byte[expectedMagic.length];
-		
-		inputStream.read(magicBytes, 0, expectedMagic.length);
+
+		ByteStreams.readFully(inputStream, magicBytes, 0, expectedMagic.length);
 
 		if(! Arrays.equals(magicBytes, expectedMagic)) {
 			throw new StorageManagerException("File " + file + " does not contain the magic bytes");
@@ -136,14 +138,14 @@ public class WriteAheadLogReader implements Closeable, Iterable<Tuple> {
 	public Iterator<Tuple> iterator() {
 		return new TupleIterator();
 	}
-	
+
 	/**
 	 * Delete the base file
 	 * @throws IOException
 	 */
 	public void deleteFile() throws IOException {
 		close();
-		
+
 		if(file.exists()) {
 			file.delete();
 		}
