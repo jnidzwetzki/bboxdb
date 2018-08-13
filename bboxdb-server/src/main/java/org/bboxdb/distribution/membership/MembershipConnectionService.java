40e185d31dccf518f2047aaee8aa1177a70019a5
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-08-13 20:42:50 +0200

    Small code refactor

--- bboxdb-server/src/main/java/org/bboxdb/distribution/membership/MembershipConnectionService.java
+++ bboxdb-server/src/main/java/org/bboxdb/distribution/membership/MembershipConnectionService.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  *
  *    Copyright (C) 2015-2018 the BBoxDB project
- *  
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
- *    limitations under the License. 
- *    
+ *    limitations under the License.
+ *
  *******************************************************************************/
 package org.bboxdb.distribution.membership;
 
@@ -34,64 +34,64 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MembershipConnectionService implements BBoxDBService {
-	
+
 	/**
 	 * The server connections
 	 */
 	private final Map<InetSocketAddress, BBoxDBConnection> serverConnections;
-	
+
 	/**
 	 * The known instances
 	 */
 	private final Map<InetSocketAddress, BBoxDBInstance> knownInstances;
-	
+
 	/**
 	 * The blacklisted instances, no connection will be created to these systems
 	 */
 	private final Set<InetSocketAddress> blacklist;
-	
+
 	/**
 	 * Is the paging for queries enabled?
 	 */
 	private boolean pagingEnabled;
-	
+
 	/**
 	 * The amount of tuples per page
 	 */
 	private short tuplesPerPage;
-	
+
 	/**
 	 * The tuple store manager registry (used for gossip, between server<->server connections)
 	 */
 	private TupleStoreManagerRegistry tupleStoreManagerRegistry;
-	
+
 	/**
 	 * The event handler
 	 */
-	private BiConsumer<DistributedInstanceEvent, BBoxDBInstance> distributedEventConsumer 
+	private BiConsumer<DistributedInstanceEvent, BBoxDBInstance> distributedEventConsumer
 		= this::handleDistributedEvent;
-	
+
 	/**
 	 * The singleton instance
 	 */
 	private static MembershipConnectionService instance = null;
-	
+
 	/**
 	 * The Logger
 	 */
 	private final static Logger logger = LoggerFactory.getLogger(MembershipConnectionService.class);
-	
+
 	private MembershipConnectionService() {
-		this.serverConnections = new ConcurrentHashMap<>();		
+		this.serverConnections = new ConcurrentHashMap<>();
 		this.knownInstances = new ConcurrentHashMap<>();
 		this.pagingEnabled = false;
 		this.tuplesPerPage = 0;
 		this.blacklist = new HashSet<>();
 	}
-	
+
 	/**
-	* Handle membership events	
-	 * @param instance 
+	* Handle membership events
+	 * @param instance
 	*/
 	private void handleDistributedEvent(final DistributedInstanceEvent event, final BBoxDBInstance instance) {
 		if(event == DistributedInstanceEvent.ADD) {
@@ -102,7 +102,7 @@ public class MembershipConnectionService implements BBoxDBService {
 			terminateConnection(instance);
 		} else {
 			logger.warn("Unknown event: " + event);
-		}		
+		}
 	}
 
 	/**
@@ -113,10 +113,10 @@ public class MembershipConnectionService implements BBoxDBService {
 		if(instance == null) {
 			instance = new MembershipConnectionService();
 		}
-		
+
 		return instance;
 	}
-	
+
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
 		throw new CloneNotSupportedException("Unable to clone a singleton");
@@ -129,28 +129,28 @@ public class MembershipConnectionService implements BBoxDBService {
 	public void addSystemToBlacklist(final BBoxDBInstance distributedInstance) {
 		blacklist.add(distributedInstance.getInetSocketAddress());
 	}
-	
+
 	/**
 	 * Clear the blacklist
 	 */
 	public void clearBlacklist() {
 		blacklist.clear();
 	}
-	
+
 	/**
 	 * Init the subsystem
 	 */
 	@Override
 	public void init() {
 		BBoxDBInstanceManager.getInstance().registerListener(distributedEventConsumer);
-		
+
 		// Create connections to existing instances
 		final List<BBoxDBInstance> instances = BBoxDBInstanceManager.getInstance().getInstances();
-		
+
 		if(instances.isEmpty()) {
 			logger.warn("The list of instances is empty");
 		}
-		
+
 		instances.forEach(i -> createOrTerminateConnetion(i));
 	}
 
@@ -160,7 +160,7 @@ public class MembershipConnectionService implements BBoxDBService {
 	@Override
 	public void shutdown() {
 		BBoxDBInstanceManager.getInstance().removeListener(distributedEventConsumer);
-		
+
 		// Close all connections
 		synchronized (serverConnections) {
 			for(final InetSocketAddress instance : serverConnections.keySet()) {
@@ -168,7 +168,7 @@ public class MembershipConnectionService implements BBoxDBService {
 				logger.info("Closing connection to server: " + instance);
 				client.disconnect();
 			}
-			
+
 			serverConnections.clear();
 			knownInstances.clear();
 		}
@@ -181,15 +181,15 @@ public class MembershipConnectionService implements BBoxDBService {
 	public String getServicename() {
 		return "Membership Connection Service";
 	}
-	
+
 	/**
 	 * Add a new connection to a bboxdb system
 	 * @param distributedInstance
 	 */
 	private synchronized void createOrTerminateConnetion(final BBoxDBInstance distributedInstance) {
 
-		// Create only connections to readonly or readwrite systems
-		if(distributedInstance.getState() == BBoxDBInstanceState.FAILED) {			
+		// Create only connections to active systems
+		if(distributedInstance.getState() == BBoxDBInstanceState.FAILED) {
 			terminateConnection(distributedInstance);
 		} else {
 			createConnection(distributedInstance);
@@ -201,30 +201,30 @@ public class MembershipConnectionService implements BBoxDBService {
 	 * @param distributedInstance
 	 */
 	private void createConnection(final BBoxDBInstance distributedInstance) {
-		
+
 		final String instanceName = distributedInstance.getStringValue();
-		
+
 		if(serverConnections.containsKey(distributedInstance.getInetSocketAddress())) {
 			logger.info("We have already a connection to: {}", instanceName);
 			return;
 		}
-		
+
 		if(blacklist.contains(distributedInstance.getInetSocketAddress())) {
 			logger.info("Not creating a connection to the blacklisted system: {}", instanceName);
 			return;
 		}
-		
+
 		logger.info("Opening connection to instance: {}", instanceName);
-		
+
 		final BBoxDBConnection connection = new BBoxDBConnection(distributedInstance.getInetSocketAddress());
 		final BBoxDBClient client = connection.getBboxDBClient();
-		
+
 		client.setPagingEnabled(pagingEnabled);
 		client.setTuplesPerPage(tuplesPerPage);
 		client.setTupleStoreManagerRegistry(tupleStoreManagerRegistry);
-		
+
 		final boolean result = connection.connect();
-		
+
 		if(! result) {
 			logger.info("Unable to open connection to: {}", instanceName);
 		} else {
@@ -233,21 +233,21 @@ public class MembershipConnectionService implements BBoxDBService {
 			knownInstances.put(distributedInstance.getInetSocketAddress(), distributedInstance);
 		}
 	}
-	
+
 	/**
 	 * Terminate the connection to a missing bboxdb system
-	 * @param distributedInstance 
+	 * @param distributedInstance
 	 */
 	private synchronized void terminateConnection(final BBoxDBInstance distributedInstance) {
-		
+
 		final String instanceName = distributedInstance.getStringValue();
-		
+
 		if(! serverConnections.containsKey(distributedInstance.getInetSocketAddress())) {
 			return;
 		}
-		
+
 		logger.info("Closing connection to dead instance: {}", instanceName);
-		
+
 		knownInstances.remove(distributedInstance.getInetSocketAddress());
 		final BBoxDBConnection connection = serverConnections.remove(distributedInstance.getInetSocketAddress());
 		connection.terminateConnection();
@@ -261,7 +261,7 @@ public class MembershipConnectionService implements BBoxDBService {
 	public BBoxDBConnection getConnectionForInstance(final BBoxDBInstance instance) {
 		return serverConnections.get(instance.getInetSocketAddress());
 	}
-	
+
 	/**
 	 * Return the number of connections
 	 * @return
@@ -269,23 +269,23 @@ public class MembershipConnectionService implements BBoxDBService {
 	public int getNumberOfConnections() {
 		return serverConnections.size();
 	}
-	
+
 	/**
 	 * Get all connections
-	 * @return 
+	 * @return
 	 */
 	public List<BBoxDBConnection> getAllConnections() {
 		return new ArrayList<>(serverConnections.values());
 	}
-	
+
 	/**
 	 * Get a list with all distributed instances we have connections to
 	 * @return
 	 */
 	public List<BBoxDBInstance> getAllInstances() {
-		return new ArrayList<BBoxDBInstance>(knownInstances.values());
+		return new ArrayList<>(knownInstances.values());
 	}
-	
+
 	/**
 	 * Is the paging for queries enables
 	 * @return
@@ -334,5 +334,5 @@ public class MembershipConnectionService implements BBoxDBService {
 	 */
 	public void setTupleStoreManagerRegistry(TupleStoreManagerRegistry tupleStoreManagerRegistry) {
 		this.tupleStoreManagerRegistry = tupleStoreManagerRegistry;
-	}	
+	}
 }
