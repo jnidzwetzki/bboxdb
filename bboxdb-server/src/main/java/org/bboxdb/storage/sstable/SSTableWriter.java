33f34963fe3be67e98d3f092372e52ae7f8a1818
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-08-24 14:22:05 +0200

    Changed logger to debug

--- bboxdb-server/src/main/java/org/bboxdb/storage/sstable/SSTableWriter.java
+++ bboxdb-server/src/main/java/org/bboxdb/storage/sstable/SSTableWriter.java
@@ -197,7 +197,7 @@ public class SSTableWriter implements AutoCloseable {
 		}
 		
 		try {
-			logger.info("Writing new SSTable for relation: {} file: {}", name.getFullname(), sstableOutputFileName);
+			logger.debug("Writing new SSTable for relation: {} file: {}", name.getFullname(), sstableOutputFileName);
 			final BufferedOutputStream sstableFileOutputStream = new BufferedOutputStream(new FileOutputStream(sstableFile));
 			sstableOutputStream = new CountingOutputStream(sstableFileOutputStream);
 			sstableOutputStream.write(SSTableConst.MAGIC_BYTES_SSTABLE);
