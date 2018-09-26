af56537ed2e6dc9bd8991fe29bc17eb508097eec
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-09-26 19:53:01 +0200

    Speed up bounding boxes creation

--- bboxdb-server/src/main/java/org/bboxdb/storage/sstable/spatialindex/rtree/RTreeDirectoryNode.java
+++ bboxdb-server/src/main/java/org/bboxdb/storage/sstable/spatialindex/rtree/RTreeDirectoryNode.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  *
  *    Copyright (C) 2015-2018 the BBoxDB project
- *  
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
- *    limitations under the License. 
- *    
+ *    limitations under the License.
+ *
  *******************************************************************************/
 package org.bboxdb.storage.sstable.spatialindex.rtree;
 
@@ -27,27 +27,27 @@ import org.bboxdb.storage.sstable.spatialindex.BoundingBoxEntity;
 import org.bboxdb.storage.sstable.spatialindex.SpatialIndexEntry;
 
 public class RTreeDirectoryNode implements BoundingBoxEntity {
-	
+
 	/**
 	 * The directory node childs
 	 */
 	protected final List<RTreeDirectoryNode> directoryNodeChilds;
-	
+
 	/**
 	 * The leaf node childs
 	 */
 	protected final List<SpatialIndexEntry> indexEntries;
-	
+
 	/**
 	 * The root node 'root' reference
 	 */
 	protected final static RTreeDirectoryNode PARENT_ROOT = null;
-	
+
 	/**
 	 * The bounding box of the node
 	 */
 	protected Hyperrectangle boundingBox;
-	
+
 	/**
 	 * The parent node
 	 */
@@ -57,7 +57,7 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 	 * The id of the node
 	 */
 	protected final int nodeId;
-	
+
 	public RTreeDirectoryNode(final int nodeId) {
 		this.parentNode = PARENT_ROOT;
 		this.nodeId = nodeId;
@@ -73,7 +73,7 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 	public Hyperrectangle getBoundingBox() {
 		return boundingBox;
 	}
-	
+
 	/**
 	 * Set the bounding box
 	 * @param boundingBox
@@ -81,7 +81,7 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 	public void setBoundingBox(Hyperrectangle boundingBox) {
 		this.boundingBox = boundingBox;
 	}
-	
+
 	/**
 	 * Get the parent node
 	 * @return
@@ -89,7 +89,7 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 	public RTreeDirectoryNode getParentNode() {
 		return parentNode;
 	}
-	
+
 	/**
 	 * Set a new parent node
 	 * @param parentNode
@@ -108,13 +108,13 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 	 * @param rTreeDirectoryNode
 	 */
 	public void addDirectoryNodeChild(final RTreeDirectoryNode rTreeDirectoryNode) {
-		
+
 		// We can carry directory nodes or index entries
 		assert (indexEntries.isEmpty());
-		
+
 		directoryNodeChilds.add(rTreeDirectoryNode);
 	}
-	
+
 	/**
 	 * Remove child directory node
 	 * @param rTreeDirectoryNode
@@ -123,51 +123,58 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 	public boolean removeDirectoryNodeChild(final RTreeDirectoryNode rTreeDirectoryNode) {
 		return directoryNodeChilds.remove(rTreeDirectoryNode);
 	}
-	
+
 	/**
-	 * Remove child leaf node 
+	 * Remove child leaf node
 	 * @param rTreeLeafNode
 	 * @return
 	 */
 	public boolean removeIndexEntry(final BoundingBoxEntity entry) {
-		
+
 		// We can carry directory nodes or index entries
 		assert (directoryNodeChilds.isEmpty());
-		
+
 		return indexEntries.remove(entry);
 	}
-	
+
 	/**
 	 * Recalculate the bounding box of all entries
 	 */
 	public void updateBoundingBox() {
 		final List<Hyperrectangle> boundingBoxes = getAllChildBoundingBoxes();
-		
+
 		// Calculate bounding box
 		this.boundingBox = Hyperrectangle.getCoveringBox(boundingBoxes);
 	}
 
 	/**
-	 * Get the bounding boxes of all childs
+	 * Get the bounding boxes of all children
 	 * @return
 	 */
 	public List<Hyperrectangle> getAllChildBoundingBoxes() {
 
-		// Get all Bounding boxes
-		return Stream.concat(directoryNodeChilds.stream(), indexEntries.stream())
-				.map(b -> b.getBoundingBox())
-				.collect(Collectors.toList());
+		final List<Hyperrectangle> resultList = new ArrayList<>();
+
+		for(final RTreeDirectoryNode node : directoryNodeChilds) {
+			resultList.add(node.getBoundingBox());
+		}
+
+		for(final SpatialIndexEntry node : indexEntries) {
+			resultList.add(node.getBoundingBox());
+		}
+
+		return resultList;
 	}
-	
+
 	/**
-	 * Find the best node for insert 
+	 * Find the best node for insert
 	 * @param entryBox
 	 * @return
 	 */
 	protected RTreeDirectoryNode findBestNodeForInsert(final Hyperrectangle entryBox) {
 		RTreeDirectoryNode bestNode = null;
 		double bestEnlargement = -1;
-		
+
 		for(final RTreeDirectoryNode node : directoryNodeChilds) {
 			final Hyperrectangle nodeBoundingBox = node.getBoundingBox();
 			final double nodeEnlargement = nodeBoundingBox.calculateEnlargement(entryBox);
@@ -177,13 +184,13 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 				bestEnlargement = nodeEnlargement;
 				continue;
 			}
-			
+
 			if(nodeEnlargement < bestEnlargement) {
 				bestNode = node;
 				bestEnlargement = nodeEnlargement;
 				continue;
 			}
-			
+
 			if(nodeEnlargement == bestEnlargement) {
 				if(bestNode.getSize() > node.getSize()) {
 					bestNode = node;
@@ -192,7 +199,7 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 				}
 			}
 		}
-		
+
 		return bestNode;
 	}
 
@@ -202,17 +209,17 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 	 * @return
 	 */
 	public List<SpatialIndexEntry> getEntriesForRegion(final Hyperrectangle boundingBox) {
-		
+
 		assert(boundingBox != null) : "Query bounding box has to be != null";
 		assert(indexEntries != null) : "Index entries has to be != null";
 		assert(directoryNodeChilds != null) : "Directory node childs has to be != null";
-		
+
 		try {
 		final List<SpatialIndexEntry> nodeMatches = indexEntries
 			.stream()
 			.filter(c -> c.getBoundingBox().intersects(boundingBox))
 			.collect(Collectors.toList());
-		
+
 		final List<SpatialIndexEntry> childMatches = directoryNodeChilds
 			.stream()
 			.filter(c -> c.getBoundingBox().intersects(boundingBox))
@@ -228,28 +235,28 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Test the bounding box covering (useful for test purposes)
 	 */
 	public void testCovering() {
-		
+
 		boolean success = true;
-		
+
 		for(final SpatialIndexEntry entry : indexEntries) {
 			if(! boundingBox.isCovering(entry.getBoundingBox())) {
 				System.err.println("Error 1");
 				success = false;
 			}
-			
+
 			if(! boundingBox.intersects(entry.getBoundingBox())) {
 				System.err.println("Error 2");
 				success = false;
 			}
 		}
-		
+
 		for(final RTreeDirectoryNode entry : directoryNodeChilds) {
-			
+
 			assert boundingBox != null;
 			assert entry.getBoundingBox() != null : "Null BBox: " + entry;
 
@@ -257,27 +264,27 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 				System.err.println("Error 3a: " + boundingBox + " does not cover" + entry.getBoundingBox());
 				entry.updateBoundingBox();
 				updateBoundingBox();
-				
+
 				if(! boundingBox.isCovering(entry.getBoundingBox())) {
 					System.err.println("Error 3b: " + boundingBox + " does not cover" + entry.getBoundingBox());
 					System.err.println(getAllChildBoundingBoxes());
 					success = false;
 				}
 			}
-			
+
 			if(! boundingBox.intersects(entry.getBoundingBox())) {
 				System.err.println("Error 4");
 				success = false;
 			}
-			
+
 			entry.testCovering();
 		}
-		
+
 		if(! success) {
 			throw new RuntimeException();
 		}
 	}
-	
+
 	/**
 	 * Get the size of the node
 	 * @return
@@ -285,14 +292,14 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 	public int getSize() {
 		return indexEntries.size() + directoryNodeChilds.size();
 	}
-	
+
 	/**
 	 * Is this a leaf node
 	 */
 	public boolean isLeafNode() {
 		return directoryNodeChilds.isEmpty();
 	}
-	
+
 	/**
 	 * Get the directory node childs
 	 * @return
@@ -300,7 +307,7 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 	public List<RTreeDirectoryNode> getDirectoryNodeChilds() {
 		return directoryNodeChilds;
 	}
-	
+
 	/**
 	 * Get the index entries
 	 * @return
@@ -336,7 +343,7 @@ public class RTreeDirectoryNode implements BoundingBoxEntity {
 			return false;
 		return true;
 	}
-	
+
 	/**
 	 * Get the node id
 	 * @return
