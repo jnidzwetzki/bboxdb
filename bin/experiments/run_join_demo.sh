bdaaddc89b64f5e0cb415262a31d9d51b1f477b3
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-10-23 22:33:27 +0200

    Added delete call

--- bin/experiments/run_join_demo.sh
+++ bin/experiments/run_join_demo.sh
@@ -33,11 +33,13 @@ source $BBOXDB_HOME/bin/bootstrap.sh
 #grep name /export/homes/nidzwetzki/osm-germany/berlin/osm/ROAD > /export/homes/nidzwetzki/osm-germany/berlin/osm/ROAD_NAME
 #grep name /export/homes/nidzwetzki/osm-germany/berlin/osm/WOOD > /export/homes/nidzwetzki/osm-germany/berlin/osm/WOOD_NAME
 
+$BBOXDB_HOME/bin/cli.sh -action delete_dgroup -dgroup osmgroup
 $BBOXDB_HOME/bin/cli.sh -action create_dgroup -dgroup osmgroup -replicationfactor 1 -dimensions 2
 $BBOXDB_HOME/bin/cli.sh -action create_table -table osmgroup_road
 $BBOXDB_HOME/bin/cli.sh -action create_table -table osmgroup_forrest
 $BBOXDB_HOME/bin/cli.sh -action import -file /export/homes/nidzwetzki/osm-germany/berlin/osm/ROAD_NAME  -format geojson -table osmgroup_road
 $BBOXDB_HOME/bin/cli.sh -action import -file /export/homes/nidzwetzki/osm-germany/berlin/osm/WOOD_NAME  -format geojson -table osmgroup_forrest
+
 
 $BBOXDB_HOME/bin/cli.sh -action query -table osmgroup_road -bbox  52.520,52.525:13.410,13.415
 
