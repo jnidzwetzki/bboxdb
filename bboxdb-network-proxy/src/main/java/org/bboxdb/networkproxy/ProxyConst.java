92e2a18684d1d8fc2a62f79cbcb0d11333f79048
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-12-17 19:47:10 +0100

    Added follow messag

--- bboxdb-network-proxy/src/main/java/org/bboxdb/networkproxy/ProxyConst.java
+++ bboxdb-network-proxy/src/main/java/org/bboxdb/networkproxy/ProxyConst.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  *
  *    Copyright (C) 2015-2018 the BBoxDB project
- *  
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
- *    limitations under the License. 
- *    
+ *    limitations under the License.
+ *
  *******************************************************************************/
 package org.bboxdb.networkproxy;
 
@@ -23,15 +23,20 @@ public class ProxyConst {
 	 * The proxy port
 	 */
 	public final static int PROXY_PORT = 10051;
-	
+
 	/**
-	 * Result OK
+	 * Result - OK
 	 */
 	public final static String RESULT_OK = "OK";
-	
+
 	/**
-	 * Result failed
+	 * Result - Another object will follow
+	 */
+	public final static String RESULT_FOLLOW = "FOLLOW";
+
+	/**
+	 * Result - failed
 	 */
 	public final static String RESULT_FAILED = "FAILED";
-	
+
 }
