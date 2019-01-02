dda5f691a71231a0a3daa9776f4dc767133609e6
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2019-01-02 20:15:42 +0100

    Changed unit test

--- bboxdb-server/src/test/java/org/bboxdb/test/network/NetworkQueryHelper.java
+++ bboxdb-server/src/test/java/org/bboxdb/test/network/NetworkQueryHelper.java
@@ -257,8 +257,8 @@ public class NetworkQueryHelper {
 		Assert.assertEquals(2, foundTuple.getNumberOfTuples());
 		Assert.assertEquals(table, foundTuple.getTupleStoreName(0));
 		Assert.assertEquals(table, foundTuple.getTupleStoreName(1));
-		Assert.assertEquals(storedTuple, foundTuple.getTuple(0));
-		Assert.assertEquals(tuple, foundTuple.getTuple(1));
+		Assert.assertEquals(tuple, foundTuple.getTuple(0));
+		Assert.assertEquals(storedTuple, foundTuple.getTuple(1));
 
 		bboxDBClient.cancelQuery(queryFuture);
 
