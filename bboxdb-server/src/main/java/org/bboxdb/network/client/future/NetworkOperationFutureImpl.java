873c7642d2ee7e0a8ebfa4fc1d808568773ec005
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2019-01-07 13:53:56 +0100

    Don't send  futures to server of connection is closed

--- bboxdb-server/src/main/java/org/bboxdb/network/client/future/NetworkOperationFutureImpl.java
+++ bboxdb-server/src/main/java/org/bboxdb/network/client/future/NetworkOperationFutureImpl.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  *
  *    Copyright (C) 2015-2018 the BBoxDB project
- *  
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
- *    limitations under the License. 
- *    
+ *    limitations under the License.
+ *
  *******************************************************************************/
 package org.bboxdb.network.client.future;
 
@@ -32,52 +32,52 @@ import org.slf4j.LoggerFactory;
 import com.google.common.base.Stopwatch;
 
 public class NetworkOperationFutureImpl implements NetworkOperationFuture {
-	
+
 	/**
 	 * The id of the operation
 	 */
 	private short requestId;
-	
+
 	/**
 	 * The result of the operation
 	 */
 	private volatile Object operationResult = null;
-	
+
 	/**
 	 * The latch for sync operations
 	 */
 	private final CountDownLatch latch = new CountDownLatch(1);
-	
+
 	/**
 	 * The error flag for the operation
 	 */
 	private volatile boolean failed = false;
-	
+
 	/**
 	 * The done flag
 	 */
 	private volatile boolean done = false;
-	
+
 	/**
 	 * The complete / partial result flag
 	 */
 	private volatile boolean complete = true;
-	
+
 	/**
 	 * Additional message
 	 */
 	private String message;
-	
+
 	/**
 	 * The future start time
 	 */
 	private final Stopwatch stopwatch;
-	
+
 	/**
 	 * The associated connection
 	 */
 	private final BBoxDBConnection connection;
-	
+
 	/**
 	 * The package supplier
 	 */
@@ -87,17 +87,17 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 	 * The executions
 	 */
 	private final AtomicInteger executions = new AtomicInteger(0);
-	
+
 	/**
 	 * The last send package
 	 */
 	private NetworkRequestPackage lastTransmittedPackage;
-	
+
 	/**
 	 * The success callback
 	 */
 	protected Consumer<NetworkOperationFuture> successCallback;
-	
+
 	/**
 	 * The error callback
 	 */
@@ -111,9 +111,9 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 	/**
 	 * Empty constructor
 	 */
-	public NetworkOperationFutureImpl(final BBoxDBConnection connection, 
+	public NetworkOperationFutureImpl(final BBoxDBConnection connection,
 			final Supplier<NetworkRequestPackage> packageSupplier) {
-		
+
 		this.packageSupplier = packageSupplier;
 		this.stopwatch = Stopwatch.createStarted();
 		this.connection = connection;
@@ -131,20 +131,28 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 	 * @see org.bboxdb.network.client.future.NetworkOperationFuture#execute()
 	 */
 	@Override
-	public void execute() {		
+	public void execute() {
+
+		if(! connection.isConnected()) {
+			logger.error("Don't execute future because connection is closed: {}", this);
+			setFailedState();
+			fireCompleteEvent();
+			return;
+		}
+
 		this.lastTransmittedPackage = packageSupplier.get();
-		this.failed = false;		
+		this.failed = false;
 		this.executions.incrementAndGet();
-		
+
 		// Can be null in some unit tests
 		if(lastTransmittedPackage != null) {
 			this.requestId = lastTransmittedPackage.getSequenceNumber();
 		}
-		
+
 		connection.registerPackageCallback(lastTransmittedPackage, this);
 		connection.sendPackageToServer(lastTransmittedPackage, this);
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see org.bboxdb.network.client.future.NetworkOperationFuture#get()
 	 */
@@ -161,11 +169,11 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 	public Object get(final long timeout, final TimeUnit unit) throws InterruptedException, TimeoutException {
 
 		latch.await(timeout, unit);
-		
+
 		if(! done) {
 			throw new TimeoutException("Unable to receive data in " + timeout + " " + unit);
 		}
-				
+
 		return operationResult;
 	}
 
@@ -206,29 +214,29 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 	 */
 	@Override
 	public void fireCompleteEvent() {
-		
+
 		// Is already be done
 		if(done) {
 			return;
 		}
-				
+
 		// Run error handler
 		if(errorCallback != null && failed) {
 			final boolean couldBeHandled = errorCallback.handleError(this);
 			if(couldBeHandled) {
 				failed = false;
 				return;
-			}						
+			}
 		}
-				
+
 		done = true;
 		stopwatch.stop();
 		latch.countDown();
-		
+
 		// Run success handler
 		if(successCallback != null) {
 			successCallback.accept(this);
-		}		
+		}
 	}
 
 	/* (non-Javadoc)
@@ -278,7 +286,7 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 		if (! isDone()) {
 			throw new IllegalArgumentException("The future is not done. Unable to calculate completion time");
 		}
-		
+
 		return stopwatch.elapsed(timeUnit);
 	}
 
@@ -289,7 +297,7 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 	public BBoxDBConnection getConnection() {
 		return connection;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see org.bboxdb.network.client.future.NetworkOperationFuture#getTransmittedPackage()
 	 */
@@ -307,17 +315,17 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 		sb.append("[message=");
 		sb.append(getMessage());
 		sb.append(", connection=");
-		
+
 		if(getConnection() == null) {
 			sb.append("null");
 		} else {
 			sb.append(connection.getConnectionName());
 		}
-		
+
 		sb.append("]");
 		return sb.toString();
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see org.bboxdb.network.client.future.NetworkOperationFuture#setErrorCallback(org.bboxdb.network.client.future.FutureErrorCallback)
 	 */
@@ -325,7 +333,7 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 	public void setErrorCallback(final FutureErrorCallback errorCallback) {
 		this.errorCallback = errorCallback;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see org.bboxdb.network.client.future.NetworkOperationFuture#setSuccessCallback(java.util.function.Consumer)
 	 */
@@ -333,7 +341,7 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 	public void setSuccessCallback(final Consumer<NetworkOperationFuture> successCallback) {
 		this.successCallback = successCallback;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see org.bboxdb.network.client.future.NetworkOperationFuture#getExecutions()
 	 */
@@ -341,5 +349,5 @@ public class NetworkOperationFutureImpl implements NetworkOperationFuture {
 	public int getExecutions() {
 		return executions.get();
 	}
-	
+
 }
