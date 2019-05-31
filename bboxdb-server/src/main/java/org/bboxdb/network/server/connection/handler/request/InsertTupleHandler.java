b97c30f27ac113e6d2e90a121644593ab13a402a
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2019-05-31 20:06:11 +0200

    Show full exceptions

--- bboxdb-server/src/main/java/org/bboxdb/network/server/connection/handler/request/InsertTupleHandler.java
+++ bboxdb-server/src/main/java/org/bboxdb/network/server/connection/handler/request/InsertTupleHandler.java
@@ -257,7 +257,6 @@ public class InsertTupleHandler implements RequestHandler {
 		} catch (RejectedException e) {
 			throw e;
 		} catch (Throwable e) {
-			logger.error("Got an exception", e);
 			throw new RejectedException(e);
 		}
 	}
