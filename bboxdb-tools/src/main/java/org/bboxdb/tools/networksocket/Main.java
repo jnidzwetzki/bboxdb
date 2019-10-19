a74d52a700e62f0e4eabdbda263a6abb1fe3c786
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2019-10-19 21:32:57 +0200

    Update Network importer

---
+++ bboxdb-tools/src/main/java/org/bboxdb/tools/networksocket/Main.java
@@ -0,0 +1,144 @@
+/*******************************************************************************
+ *
+ *    Copyright (C) 2015-2018 the BBoxDB project
+ *  
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License. 
+ *    
+ *******************************************************************************/
+package org.bboxdb.tools.networksocket;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.bboxdb.commons.InputParseException;
+import org.bboxdb.commons.MathUtil;
+import org.bboxdb.network.client.BBoxDB;
+import org.bboxdb.network.client.BBoxDBCluster;
+import org.bboxdb.network.client.future.client.EmptyResultFuture;
+import org.bboxdb.network.client.tools.FixedSizeFutureStore;
+import org.bboxdb.storage.entity.Tuple;
+import org.bboxdb.tools.converter.tuple.TupleBuilder;
+import org.bboxdb.tools.converter.tuple.TupleBuilderFactory;
+
+public class Main implements Runnable {
+
+	/**
+	 * The port to open the server socket
+	 */
+	private int port;
+	
+	/**
+	 * The BBoxDB connection point
+	 */
+	private String connectionPoint;
+	
+	/**
+	 * The clustername
+	 */
+	private String clustername;
+
+	/**
+	 * The table to insert to
+	 */
+	private String table;
+	
+	/**
+	 * The tuple decoder
+	 */
+	private TupleBuilder tupleFactory;
+	
+	/**
+	 * The pending futures
+	 */
+	private final FixedSizeFutureStore pendingFutures;
+	
+	/**
+	 * The amount of pending insert futures
+	 */
+	private final static int MAX_PENDING_FUTURES = 100;
+
+	public Main(final int port, final String connectionPoint, 
+			final String clustername, String table, final TupleBuilder tupleFactory) {
+				this.port = port;
+				this.connectionPoint = connectionPoint;
+				this.clustername = clustername;
+				this.table = table;
+				this.tupleFactory = tupleFactory;
+				this.pendingFutures = new FixedSizeFutureStore(MAX_PENDING_FUTURES);
+	}
+
+	@Override
+	public void run() {
+
+	    try(
+	    		final ServerSocket serverSocket = new ServerSocket(port);
+	    		final BBoxDB bboxdbClient = new BBoxDBCluster(connectionPoint, clustername);
+	        ) {
+			final Socket clientSocket = serverSocket.accept();
+			final InputStreamReader reader = new InputStreamReader(clientSocket.getInputStream());
+			final BufferedReader inputStream = new BufferedReader(reader);
+			
+			bboxdbClient.connect();
+			
+			long lineNumber = 0; 
+			String line;
+		    while ((line = inputStream.readLine()) != null) {
+		    	final Tuple tuple = tupleFactory.buildTuple(Long.toString(lineNumber), line);
+		    	final EmptyResultFuture result = bboxdbClient.insertTuple(table, tuple);
+		    	pendingFutures.put(result);	
+		    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			waitForPendingFutures();
+		}   
+	}
+
+	/**
+	 * Wait for the pending futures
+	 */
+	private void waitForPendingFutures() {
+		try {
+			pendingFutures.waitForCompletion();
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+	
+	/**
+	 * Main * Main * Main * Main
+	 * @param args
+	 * @throws InputParseException
+	 */
+	public static void main(final String[] args) throws InputParseException {
+		
+		if(args.length < 4) {
+			System.err.println("Usage: <Class> <Port> <Connection Endpoint> <Clustername> <Table> <Format>");
+			System.exit(-1);
+		}
+		
+		final String portString = args[0];
+		final int port = MathUtil.tryParseInt(portString, () -> "Unable to parse: " + portString);
+		final String connectionPoint = args[1];
+		final String clustername = args[2];
+		final String table = args[3];
+		final String format = args[4];
+		
+		final TupleBuilder tupleFactory = TupleBuilderFactory.getBuilderForFormat(format);
+		
+		final Main main = new Main(port, connectionPoint, clustername, table, tupleFactory);
+		main.run();
+	}
+}
