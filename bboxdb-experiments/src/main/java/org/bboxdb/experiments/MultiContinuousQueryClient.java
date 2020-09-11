9ee669d32a0e0c22f5f08a95e164ce92f687c22a
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2020-09-11 13:27:33 +0200

    Updated query generator

--- bboxdb-experiments/src/main/java/org/bboxdb/experiments/MultiContinuousQueryClient.java
+++ bboxdb-experiments/src/main/java/org/bboxdb/experiments/MultiContinuousQueryClient.java
@@ -17,9 +17,18 @@
  *******************************************************************************/
 package org.bboxdb.experiments;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.bboxdb.commons.MathUtil;
 import org.bboxdb.commons.math.Hyperrectangle;
@@ -53,19 +62,9 @@ public class MultiContinuousQueryClient implements Runnable {
 	private final String table;
 	
 	/**
-	 * The data range
+	 * The data ranges
 	 */
-	private final Hyperrectangle range;
-	
-	/**
-	 * The percentage of the size of the query rectangle
-	 */
-	private final double percentage;
-	
-	/**
-	 * The amount of parallel queries
-	 */
-	private final double parallelQueries;
+	private final List<Hyperrectangle> ranges;
 	
 	/**
 	 * All threads
@@ -78,15 +77,12 @@ public class MultiContinuousQueryClient implements Runnable {
 	private final static Logger logger = LoggerFactory.getLogger(MultiContinuousQueryClient.class);
 	
 	public MultiContinuousQueryClient(final String contactPoint, final String clusterName, 
-			final String table, final Hyperrectangle range, final double percentage,
-			final double parallelQueries) {
+			final String table, final List<Hyperrectangle> ranges) {
 			
 				this.contactPoint = contactPoint;
 				this.clusterName = clusterName;
 				this.table = table;
-				this.range = range;
-				this.percentage = percentage;
-				this.parallelQueries = parallelQueries;
+				this.ranges = ranges;
 	}
 	
 	@Override
@@ -95,8 +91,7 @@ public class MultiContinuousQueryClient implements Runnable {
 		try(final BBoxDB connection = new BBoxDBCluster(contactPoint, clusterName)) {
 			connection.connect();
 			
-			for(int i = 0; i < parallelQueries; i++) {
-				final Hyperrectangle queryRectangle = RandomQueryRangeGenerator.getRandomQueryRange(range, percentage);
+			for(final Hyperrectangle queryRectangle : ranges) {
 				System.out.println("Creating query in range: " + queryRectangle);
 				
 				final ContinuousQueryPlan queryPlan = QueryPlanBuilder
@@ -109,7 +104,7 @@ public class MultiContinuousQueryClient implements Runnable {
 				readResultsInThread(queryFuture);
 			}
 			
-			logger.info("Successfully registered {} queries", parallelQueries);
+			logger.info("Successfully registered {} queries", ranges.size());
 			
 			for(final Thread thread : allThreads) {
 				thread.join();
@@ -149,17 +144,114 @@ public class MultiContinuousQueryClient implements Runnable {
 	 */
 	public static void main(String[] args) {
 		
-		if(args.length != 6) {
-			System.err.println("Usage: <Class> <ClusterContactPoint> <Clusterneme> <Table> <Range> <Percentage> <Parallel-Queries>");
+		if(args.length < 2) {
+			System.err.println("Usage: <Class> Action:{autogenerate, writefile, readfile}");
 			System.exit(-1);
 		}
 		
-		final String contactPoint = args[0];
-		final String clusterName = args[1];
-		final String table = args[2];
-		final String rangeString = args[3];
-		final String percentageString = args[4];
-		final String parallelQueriesString = args[5];
+		final String operation = args[0];
+		
+		if("autogenerate".equals(operation)) {
+			performAutogenerate(args);
+		} else if("writefile".equals(operation)) {
+			performWriteData(args);
+		} else if("readfile".equals(operation)) {
+			performReadData(args);
+		} else {
+			System.err.println("Usage: <Class> Action:{autogenerate, writefile, readfile}");
+			System.exit(-1);
+		}
+	}
+
+	/**
+	 * Perform the read data operation
+	 * @param args
+	 */
+	private static void performReadData(final String[] args) {
+		if(args.length != 5) {
+			System.err.println("Usage: <Class> readfile <ClusterContactPoint> <Clustername> <Table> <File>");
+			System.exit(-1);
+		}
+		
+		final String contactPoint = args[1];
+		final String clusterName = args[2];
+		final String table = args[3];
+		final File inputFile = new File(args[4]);
+		
+		 new ArrayList<>();
+
+		try (final Stream<String> lineStream = Files.lines(Paths.get(inputFile.getPath()))) {
+	
+			final List<Hyperrectangle> ranges = lineStream
+					.map(s -> Hyperrectangle
+					.fromString(s))
+			 		.collect(Collectors.toList());
+	
+			final MultiContinuousQueryClient runable = new MultiContinuousQueryClient(contactPoint, clusterName, 
+					table, ranges);
+			
+			runable.run();
+
+		} catch (IOException e) {
+			logger.error("Got exception during write", e);
+		}
+	}
+
+	/**
+	 * Write the queries into a file for later usage
+	 * @param args
+	 */
+	private static void performWriteData(final String[] args) {
+		if(args.length != 5) {
+			System.err.println("Usage: <Class> writefile <File> <Range> <Percentage> <Parallel-Queries>");
+			System.exit(-1);
+		}
+
+		final File outputFile = new File(args[1]);
+		final String rangeString = args[2];
+		final String percentageString = args[3];
+		final String parallelQueriesString = args[4];
+		
+		if(outputFile.exists()) {
+			System.err.println("The specified output file already exists");
+			System.exit(-1);
+		}
+		
+		final Optional<Hyperrectangle> range = HyperrectangleHelper.parseBBox(rangeString);
+		
+		if(! range.isPresent()) {
+			System.err.println("Unable to parse as bounding box: " + rangeString);
+		}
+		
+		final double percentage = MathUtil.tryParseDoubleOrExit(percentageString, () -> "Unable to parse: " + percentageString);
+		final double parallelQueries = MathUtil.tryParseDoubleOrExit(parallelQueriesString, () -> "Unable to parse: " + parallelQueriesString);
+
+		try(final BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+			for(int i = 0; i < parallelQueries; i++) {
+				final Hyperrectangle queryRectangle = RandomQueryRangeGenerator.getRandomQueryRange(range.get(), percentage);
+				writer.write(queryRectangle.toCompactString() + "\n");
+			}
+		} catch (IOException e) {
+			logger.error("Got exception during write", e);
+		}
+	}
+
+	/**
+	 * Perform the autogenerate action
+	 * @param args
+	 */
+	private static void performAutogenerate(final String[] args) {
+		if(args.length != 7) {
+			System.err.println("Usage: <Class> autogenerate <ClusterContactPoint> <Clustername> <Table> <Range> <Percentage> <Parallel-Queries>");
+			System.exit(-1);
+		}
+		
+		final String contactPoint = args[1];
+		final String clusterName = args[2];
+		final String table = args[3];
+		final String rangeString = args[4];
+		final String percentageString = args[5];
+		final String parallelQueriesString = args[6];
 		
 		final Optional<Hyperrectangle> range = HyperrectangleHelper.parseBBox(rangeString);
 		
@@ -170,10 +262,16 @@ public class MultiContinuousQueryClient implements Runnable {
 		final double percentage = MathUtil.tryParseDoubleOrExit(percentageString, () -> "Unable to parse: " + percentageString);
 		final double parallelQueries = MathUtil.tryParseDoubleOrExit(parallelQueriesString, () -> "Unable to parse: " + parallelQueriesString);
 		
+		final List<Hyperrectangle> ranges = new ArrayList<>();
+
+		for(int i = 0; i < parallelQueries; i++) {
+			final Hyperrectangle queryRectangle = RandomQueryRangeGenerator.getRandomQueryRange(range.get(), percentage);
+			ranges.add(queryRectangle);
+		}
+		
 		final MultiContinuousQueryClient runable = new MultiContinuousQueryClient(contactPoint, clusterName, 
-				table, range.get(), percentage, parallelQueries);
+				table, ranges);
 		
 		runable.run();
 	}
-
 }
