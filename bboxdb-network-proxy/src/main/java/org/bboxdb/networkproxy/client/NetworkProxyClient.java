91cf2e0bc3f7f7c5a64493428c7dd90e450f76eb
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2022-01-13 15:58:42 +0100

    Update copyright message in header

--- bboxdb-network-proxy/src/main/java/org/bboxdb/networkproxy/client/NetworkProxyClient.java
+++ bboxdb-network-proxy/src/main/java/org/bboxdb/networkproxy/client/NetworkProxyClient.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *
- *    Copyright (C) 2015-2021 the BBoxDB project
+ *    Copyright (C) 2015-2022 the BBoxDB project
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
